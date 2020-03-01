3c4aff4cf79bd6c8117485cba76ff2f6201996ed
YTGhost <283304489@qq.com>
2020-03-01 22:25:34 +0800

    add 225 2020/03/01

---
+++ C_C++/225. 用队列实现栈.cpp
@@ -0,0 +1,46 @@
+class MyStack {
+public:
+    queue<int> a;
+    /** Initialize your data structure here. */
+    MyStack() {
+
+    }
+    
+    /** Push element x onto stack. */
+    void push(int x) {
+        int size = a.size();
+        a.push(x);
+        while(size--)
+        {
+            int temp = a.front();
+            a.pop();
+            a.push(temp);
+        }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    int pop() {
+        int temp = a.front();
+        a.pop();
+        return temp;
+    }
+    
+    /** Get the top element. */
+    int top() {
+        return a.front();
+    }
+    
+    /** Returns whether the stack is empty. */
+    bool empty() {
+        return a.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
