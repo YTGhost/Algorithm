32e62d537f86daf856f2642e52001c8046e6a8f8
YTGhost <283304489@qq.com>
2020-03-12 22:49:15 +0800

    add 1071 2020/03/12

---
+++ C_C++/1071. 字符串的最大公因子.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        if(str1 + str2 != str2 + str1)  return "";
+        return str1.substr(0, gcd(str1.length(), str2.length()));
+    }
+    int gcd(int a, int b){return b == 0 ? a : gcd(b, a%b);}
+};
