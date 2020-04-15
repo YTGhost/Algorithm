126752d5ca5f7191cdc7e5fd428de8e132437227
YTGhost <283304489@qq.com>
2020-04-16 07:23:58 +0800

    add 56 2020/04/16

---
+++ C_C++/56. 合并区间.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size() == 1 || intervals.size() == 0) return intervals;
+        vector<vector<int>> ans;
+        sort(intervals.begin(), intervals.end());
+        ans.push_back(intervals[0]);
+        for(int i = 1; i < intervals.size(); i++)
+        {
+            if(ans.back()[1] >= intervals[i][0])
+            {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }else{
+                ans.push_back(intervals[i]);
+            }
+        }
+        return ans;
+    }
+};
