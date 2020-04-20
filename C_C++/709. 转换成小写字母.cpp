d2e11edb5d63887e794b11422e60b74aea185233
YTGhost <283304489@qq.com>
2020-04-20 15:50:42 +0800

    add 344 709 2020/04/20

---
+++ C_C++/709. 转换成小写字母.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str[i] >= 'A' && str[i] <= 'Z') str[i] = 'a' + str[i] - 'A';
+        }
+        return str;
+    }
+};
