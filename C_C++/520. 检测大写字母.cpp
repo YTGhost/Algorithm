db123ff0bd8eb4bad563c7ff9c66041257c518d4
YTGhost <283304489@qq.com>
2020-04-21 14:25:24 +0800

    add 520 2020/04/21

---
+++ C_C++/520. 检测大写字母.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int sign;
+        if(word.length() == 1) return true;
+        if(word[0] >= 'A' && word[0] <= 'Z')
+        {
+            if(word[1] >= 'A' && word[1] <= 'Z') sign = 1;
+            else sign = 3;
+        }else if(word[1] < 'a' || word[1] > 'z') return false;
+        else sign = 2;
+        for(int i = 2; i < word.length(); i++)
+        {
+            char t = word[i];
+            if(sign == 1 && (word[i] < 'A' || word[i] >'Z')) return false;
+            else if((sign == 2 || sign == 3) && (word[i] < 'a' || word[i] > 'z')) return false;
+        }
+        return true;
+    }
+};
