02da0f1cf11b51d4a97485d1f0d4d27d9d95298b
YTGhost <283304489@qq.com>
2020-04-23 22:22:38 +0800

    add 面试题08.11

---
+++ C_C++/面试题 08.11. 硬币.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    static constexpr int mod = 1000000007;
+    static constexpr int coins[4] = {25, 10, 5, 1};
+
+public:
+    int waysToChange(int n) {
+        vector<int> f(n + 1);
+        f[0] = 1;
+        for (int c = 0; c < 4; ++c) {
+            int coin = coins[c];
+            for (int i = coin; i <= n; ++i) {
+                f[i] = (f[i] + f[i - coin]) % mod;
+            }
+        }
+        return f[n];
+    }
+};
