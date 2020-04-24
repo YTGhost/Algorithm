8751200b9c92d9b5b02d765b2114bf08a42f0b49
YTGhost <283304489@qq.com>
2020-04-24 21:16:40 +0800

    add 389 2020/04/24

---
+++ C_C++/389. 找不同.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        int a[26];
+        int b[26];
+        char c;
+        memset(a, 0, sizeof a);
+        memset(b, 0, sizeof b);
+        for(int i = 0; i < s.length(); i++) a[s[i] - 'a']++;
+        for(int i = 0; i < t.length(); i++)
+        {
+            b[t[i] - 'a']++;
+            if(b[t[i] - 'a'] > a[t[i] - 'a']){
+                c = 'a' + (t[i] - 'a');
+                break;
+            } 
+        }
+        return c;
+    }
+};
