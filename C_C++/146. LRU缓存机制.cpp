f9d878edefcccac3f8ebf0aa6b04751c804a93f6
YTGhost <283304489@qq.com>
2020-05-25 11:07:02 +0800

    add 146 2020/05/25

---
+++ C_C++/146. LRU缓存机制.cpp
@@ -0,0 +1,88 @@
+struct DLinkedNode {
+        int key, value;
+        DLinkedNode* prev;
+        DLinkedNode* next;
+        DLinkedNode(): key(0), value(0), prev(nullptr), next(nullptr) {}
+        DLinkedNode(int _key, int _value): key(_key), value(_value), prev(nullptr), next(nullptr) {}
+    };
+
+class LRUCache {
+private:
+    unordered_map<int, DLinkedNode*> cache;
+    DLinkedNode* head;
+    DLinkedNode* tail;
+    int size;
+    int capacity;
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+        size = 0;
+        // 使用伪头部和伪尾部结点
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head->next = tail;
+        tail->prev = head;
+    }
+    
+    int get(int key) {
+        if(!cache.count(key)) return -1;
+        // 如果key存在，先通过哈希表定位，再移到头部
+        DLinkedNode* node = cache[key];
+        moveToHead(node);
+        return node->value;
+    }
+    
+    void put(int key, int value) {
+        if(!cache.count(key)){
+            // 如果key不存在，创建一个新的节点
+            DLinkedNode* node = new DLinkedNode(key, value);
+            // 添加进哈希表
+            cache[key] = node;
+            // 添加至双向链表的头部
+            addToHead(node);
+            size++;
+            if(size > capacity){
+                // 如果超出容量，删除双向链表的尾部结点
+                DLinkedNode* removed = removeTail();
+                cache.erase(removed->key);
+                size--;
+            }
+        }else{
+            // 如果key存在，就通过哈希表定位，再修改value，并移到头部
+            DLinkedNode* node = cache[key];
+            node->value = value;
+            moveToHead(node);
+        }
+    }
+
+    void addToHead(DLinkedNode* node){
+        node->prev = head;
+        node->next = head->next;
+        head->next->prev = node;
+        head->next = node;
+    }
+
+    void removeNode(DLinkedNode* node){
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    void moveToHead(DLinkedNode* node){
+        removeNode(node);
+        addToHead(node);
+    }
+
+    DLinkedNode* removeTail(){
+        DLinkedNode* node = tail->prev;
+        removeNode(node);
+        return node;
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
