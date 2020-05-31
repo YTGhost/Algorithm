c463596cc627ca35a3d6447ec15700cad894d931
YTGhost <283304489@qq.com>
2020-05-31 20:37:25 +0800

    重做101 add 5424 5425 2020/05/31

--- C_C++/101. 对称二叉树.cpp
+++ C_C++/101. 对称二叉树.cpp
@@ -29,3 +29,18 @@ public:
         return isS(root->left, root->right);
     }
 };
+
+// 5月31打卡重做
+class Solution {
+public:
+    bool check(TreeNode* left, TreeNode* right){
+        if(!left && !right) return true;
+        if(!left || !right) return false;
+        if(left->val == right->val && check(left->left, right->right) && check(left->right, right->left)) return true;
+        else return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return true;
+        return check(root->left, root->right);
+    }
+};
---
+++ C_C++/5425. 切割后面积最大的蛋糕.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        horizontalCuts.push_back(0);
+        horizontalCuts.push_back(h);
+        verticalCuts.push_back(0);
+        verticalCuts.push_back(w);
+
+        sort(horizontalCuts.begin(), horizontalCuts.end());
+        sort(verticalCuts.begin(), verticalCuts.end());
+
+        int x = 0, y = 0;
+        for(int i = 1; i < horizontalCuts.size(); i++) x = max(x, horizontalCuts[i] - horizontalCuts[i-1]);
+        for(int i = 1; i < verticalCuts.size(); i++) y = max(y, verticalCuts[i] - verticalCuts[i-1]);
+        
+        return (long long)x * y % 1000000007;
+    }
+};
