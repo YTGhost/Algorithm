bd06de2b1b8ef18ed10079a9b120d982b5e6ef49
YTGhost <283304489@qq.com>
2020-07-02 22:10:04 +0800

    add 378 2020/07/02

---
+++ C_C++/378. 有序矩阵中第K小的元素.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        vector<int> res;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                res.push_back(matrix[i][j]);
+        sort(res.begin(), res.end());
+        return res[k-1];
+    }
+};
