9d6c4fe3fd3330206d0d72ef6a2e3d83e0261e9c
YTGhost <283304489@qq.com>
2020-07-14 20:34:05 +0800

    change 120 2020/07/14

--- C_C++/120. 三角形最小路径和.cpp
+++ C_C++/120. 三角形最小路径和.cpp
@@ -32,3 +32,24 @@ public:
         return res;
     }
 };
+
+// 每日打卡重做
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        int ans = INT_MAX;
+        vector<vector<int>> f(n+1, vector<int>(n+1, 0));
+        f[0][0] = triangle[0][0];
+
+        for(int i = 1; i < n; i++)
+            for(int j = 0; j <= i; j++)
+            {
+                if(j == 0) f[i][j] = f[i-1][j] + triangle[i][j];
+                else if(j == i) f[i][j] = f[i-1][j-1] + triangle[i][j];
+                else f[i][j] = min(f[i-1][j], f[i-1][j-1]) + triangle[i][j];
+            }
+        for(int i = 0; i < n; i++) ans = min(ans, f[n-1][i]);
+        return ans;
+    }
+};
