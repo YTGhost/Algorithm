d52b8d828b71d80940c4eb5e775d5b292bf7ee2b
YTGhost <283304489@qq.com>
2020-07-20 11:51:31 +0800

    add 167 2020/07/20

---
+++ C_C++/167. 两数之和 II - 输入有序数组.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size()-1;
+        while(l < r)
+        {
+            int sum = numbers[l] + numbers[r];
+            if(sum < target) l++;
+            else if(sum > target) r--;
+            else return {l+1, r+1};
+        }
+        return {l+1, r+1};
+    }
+};
