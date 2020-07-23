fb5027e073ab8c79a4d5c72e177f2884c6b152ee
YTGhost <283304489@qq.com>
2020-07-23 09:26:31 +0800

    64每日一题打卡 2020/07/23

--- C_C++/64. 最小路径和.cpp
+++ C_C++/64. 最小路径和.cpp
@@ -1,3 +1,21 @@
+// 每日一题打卡
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> f(m, vector<int>(n, 0));
+        for(int i = 0; i < m; i++)
+            for(int j = 0; j < n; j++)
+            {
+                if(i == 0 && j == 0) f[i][j] += grid[i][j];
+                else if(i == 0) f[i][j] = f[i][j-1] + grid[i][j];
+                else if(j == 0) f[i][j] = f[i-1][j] + grid[i][j];
+                else f[i][j] = min(f[i-1][j], f[i][j-1]) + grid[i][j];
+            }
+        return f[m-1][n-1];
+    }
+};
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
