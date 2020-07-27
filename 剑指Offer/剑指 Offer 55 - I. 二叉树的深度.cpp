116d87a7e61477e7c471e7662325d9a4a6fcbc5c
YTGhost <283304489@qq.com>
2020-07-27 23:40:11 +0800

    add 剑指55 2020/07/27

---
+++ 剑指Offer/剑指 Offer 55 - I. 二叉树的深度.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root){
+        if(!root->left && !root->right) return 1;
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        return 1 + max(left, right);
+    }
+
+    int maxDepth(TreeNode* root) {
+        int res = 0;
+        if(root) res = dfs(root);
+        return res;
+    }
+};
