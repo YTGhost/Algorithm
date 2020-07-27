a545f867c5554b6e6d948a0cf4de61859d1e0a6f
YTGhost <283304489@qq.com>
2020-07-27 23:20:42 +0800

    modify 392 2020/07/27

--- C_C++/392. 判断子序列.cpp
+++ C_C++/392. 判断子序列.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        int n = s.length(), m = t.length();
+        int i = 0, j = 0;
+
+        while(i < n && j < m)
+        {
+            if(s[i] == t[j]) i++;
+            j++;
+        }
+        return i == n; 
+    }
+};
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
         if(s.length() == 0)
         {
             return true;
