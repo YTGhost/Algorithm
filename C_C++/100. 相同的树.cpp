491c11766b7ecd497896cb69fe60dfb2ecbae6a4
YTGhost <283304489@qq.com>
2020-08-07 07:08:21 +0800

    modify 100 2020/08/07

--- C_C++/100. 相同的树.cpp
+++ C_C++/100. 相同的树.cpp
@@ -22,3 +22,13 @@ public:
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };
+
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(!p || !q) return !p && !q;
+        bool left = isSameTree(p->left, q->left);
+        bool right = isSameTree(p->right, q->right);
+        return p->val == q->val && left && right;
+    }
+};
