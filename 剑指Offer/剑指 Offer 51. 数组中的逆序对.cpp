53add86664e38a3c52e582ae710244c6ad66616a
YTGhost <283304489@qq.com>
2020-08-15 23:58:38 +0800

    modify 剑指51 2020/08/15

--- 剑指Offer/剑指 Offer 51. 数组中的逆序对.cpp
+++ 剑指Offer/剑指 Offer 51. 数组中的逆序对.cpp
@@ -28,3 +28,34 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int t[50010];
+    int ans;
+    void merge_sort(vector<int>& nums, int l, int r) {
+        if(l >= r) return;
+        int mid = l + r >> 1;
+
+        merge_sort(nums, l, mid);
+        merge_sort(nums, mid+1, r);
+
+        int k = 0, i = l, j = mid+1;
+        while(i <= mid && j <= r)
+        {
+            if(nums[i] <= nums[j]) t[k++] = nums[i++];
+            else {
+                t[k++] = nums[j++];
+                ans += mid - i + 1;
+            }
+        }
+        while(i <= mid) t[k++] = nums[i++];
+        while(j <= r) t[k++] = nums[j++];
+        for(int i = l, j = 0; i <= r; i++, j++) nums[i] = t[j];
+
+    }
+    int reversePairs(vector<int>& nums) {
+        merge_sort(nums, 0, nums.size()-1);
+        return ans;
+    }
+};
