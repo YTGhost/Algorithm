b373047ee1d63ebc8275c2f01e5201d7e16e797d
YTGhost <283304489@qq.com>
2020-08-16 18:56:36 +0800

    modify 733 2020/08/16

--- C_C++/733. 图像渲染.cpp
+++ C_C++/733. 图像渲染.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+    int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+    int oldColor;
+    int newColor;
+    void dfs(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        if(sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size() || image[sr][sc] != oldColor || image[sr][sc] == newColor) {
+            return;
+        }
+        image[sr][sc] = newColor;
+        for(int i = 0; i < 4; i++) dfs(image, sr+dx[i], sc+dy[i], newColor);
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        oldColor = image[sr][sc];
+        dfs(image, sr, sc, newColor);
+        return image;
+    }
+};
+
+class Solution {
+public:
     int oldColor;
     int a[4] = {1, -1, 0, 0};
     int b[4] = {0, 0, 1, -1};
