c01963bb202918e1ba0063dbebe2a4479f1be14b
YTGhost <283304489@qq.com>
2020-08-21 22:28:08 +0800

    add 111 2020/08/21

--- C_C++/111. 二叉树的最小深度.cpp
+++ C_C++/111. 二叉树的最小深度.cpp
@@ -10,6 +10,15 @@
 class Solution {
 public:
     int minDepth(TreeNode* root) {
+        if(!root) return 0;
+        if(!root->left || !root->right) return max(minDepth(root->left), minDepth(root->right)) + 1;
+        return min(minDepth(root->left), minDepth(root->right)) + 1;
+    }
+};
+
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
         if(root == NULL){
             return 0;
         }
