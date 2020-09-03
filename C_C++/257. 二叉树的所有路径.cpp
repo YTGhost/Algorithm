9e16917858eee70726ecc067ee5ec6f6709eb0d2
YTGhost <283304489@qq.com>
2020-09-04 01:03:37 +0800

    modify 257 2020/09/04

--- C_C++/257. 二叉树的所有路径.cpp
+++ C_C++/257. 二叉树的所有路径.cpp
@@ -9,6 +9,27 @@
  */
 class Solution {
 public:
+    vector<string> res;
+    void dfs(TreeNode* root, string path) {
+        if(!root) return;
+        if(!root->left && !root->right) {
+            path += to_string(root->val);
+            res.push_back(path);
+            return;
+        }
+        path += to_string(root->val);
+        path += "->";
+        dfs(root->left, path);
+        dfs(root->right, path);
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        dfs(root, "");
+        return res;
+    }
+};
+
+class Solution {
+public:
     vector<string> ans;
     void dfs(TreeNode* root, string str){
         if(!root) return;
