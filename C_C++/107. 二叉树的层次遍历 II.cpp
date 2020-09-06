7da0b448fe7aa907f4387821548ba847dc1bc31a
YTGhost <283304489@qq.com>
2020-09-06 10:15:44 +0800

    modify 107 2020/09/06

--- C_C++/107. 二叉树的层次遍历 II.cpp
+++ C_C++/107. 二叉树的层次遍历 II.cpp
@@ -9,6 +9,31 @@
  */
 class Solution {
 public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> res;
+        if(!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size = q.size();
+            vector<int> temp;
+            while(size--)
+            {
+                auto node = q.front();
+                q.pop();
+                temp.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            res.push_back(temp);
+        }
+        return vector<vector<int>>(res.rbegin(), res.rend());
+    }
+};
+
+class Solution {
+public:
     vector<vector<int>> a;
     queue<TreeNode*> q;
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
