4b211158b7f64a3a916c5e18f5696dfd1876e41e
YTGhost <283304489@qq.com>
2020-09-14 09:18:09 +0800

    modify 94 2020/09/14

--- C_C++/94. 二叉树的中序遍历.cpp
+++ C_C++/94. 二叉树的中序遍历.cpp
@@ -7,6 +7,39 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// 迭代
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        auto cur = root;
+        stack<TreeNode*> s;
+        while(cur || !s.empty())
+        {
+            if(cur) {
+                s.push(cur);
+                cur = cur->left;
+            } else {
+                auto t = s.top();
+                s.pop();
+                res.emplace_back(t->val);
+                cur = t->right;
+            }
+        }
+        return res;
+    }
+};
+
+// 递归
 class Solution {
 public:
     vector<int> res;
