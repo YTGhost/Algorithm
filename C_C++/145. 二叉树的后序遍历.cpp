f02ec43645662d948fc429f5bde1d58715fd6bbc
YTGhost <283304489@qq.com>
2020-09-29 09:33:28 +0800

    145 增加迭代法 2020/09/29

---
+++ C_C++/145. 二叉树的后序遍历.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+//迭代（）
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty())
+        {
+            auto node = s.top();
+            res.emplace_back(node->val);
+            s.pop();
+            if(node->left) s.push(node->left);
+            if(node->right) s.push(node->right);
+        }
+        return vector<int>(res.rbegin(), res.rend());
+    }
+};
+// 递归
+class Solution {
+public:
+    vector<int> a;
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(root){
+            postorderTraversal(root->left);
+            postorderTraversal(root->right);
+            a.insert(a.end(), root->val);
+        }
+        return a;
+    }
+};
