de684f238784763615c4056138e5165c1159baac
YTGhost <283304489@qq.com>
2020-10-04 11:20:45 +0800

    modify 2 2020/10/04

--- C_C++/2. 两数相加.cpp
+++ C_C++/2. 两数相加.cpp
@@ -6,6 +6,27 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int carry = 0;
+        ListNode* dummyNode = new ListNode(0);
+        ListNode* cur = dummyNode;
+        while(carry || l1 || l2)
+        {
+            int l1Val = l1 ? l1->val : 0;
+            int l2Val = l2 ? l2->val : 0;
+            int val = l1Val + l2Val + carry;
+            ListNode* temp = new ListNode(val % 10);
+            cur->next = temp;
+            cur = cur->next;
+            carry = val / 10;
+            if(l1) l1 = l1->next;
+            if(l2) l2 = l2->next;
+        }
+        return dummyNode->next;
+    }
+};
 // LeetCode究极班打卡
 class Solution {
 public:
