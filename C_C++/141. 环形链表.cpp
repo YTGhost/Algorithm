b3ddd214ad170c14704c9d45b3398c3a7deaa1e7
YTGhost <283304489@qq.com>
2020-10-09 01:17:20 +0800

    add 141 2020/10/09

---
+++ C_C++/141. 环形链表.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(!head || !head->next) return false;
+        ListNode *slow = head, *fast = head->next;
+        while(slow != fast)
+        {
+            if(!fast || !fast->next) return false;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return fast;
+    }
+};
