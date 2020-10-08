0553d8fe8158ea74525b3f3c9594075182651be7
YTGhost <283304489@qq.com>
2020-10-08 09:00:31 +0800

    modify 344 2020/10/08

--- C_C++/344. 反转字符串.cpp
+++ C_C++/344. 反转字符串.cpp
@@ -1,3 +1,13 @@
+// 每日一题打卡
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int l = 0, r = s.size()-1;
+        while(l < r)
+            swap(s[l++], s[r--]);
+    }
+};
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
