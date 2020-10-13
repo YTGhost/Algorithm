79442863a680b0d23caac683d30ed386fc045b18
YTGhost <283304489@qq.com>
2020-10-13 13:27:44 +0800

    modify 24 2020/10/13

--- C_C++/24. 两两交换链表中的节点.cpp
+++ C_C++/24. 两两交换链表中的节点.cpp
@@ -6,6 +6,7 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+// 递归
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
@@ -14,5 +15,25 @@ public:
         head->next = swapPairs(next->next);
         next->next = head;
         return next;
+    }
+};
+
+// 迭代
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        dummy->next = head;
+        ListNode* temp = dummy;
+        while(temp->next && temp->next->next)
+        {
+            ListNode* node1 = temp->next;
+            ListNode* node2 = temp->next->next;
+            temp->next = node2;
+            node1->next = node2->next;
+            node2->next = node1;
+            temp = node1;
+        }
+        return dummy->next;
     }
 };
