a9da6b5b5c223949d3c735191c2ae0d2c745db8f
YTGhost <283304489@qq.com>
2020-10-17 22:00:21 +0800

    modify 52 2020/10/17

--- C_C++/52. N皇后 II.cpp
+++ C_C++/52. N皇后 II.cpp
@@ -1,3 +1,34 @@
+// 每日一题
+class Solution {
+public:
+    int res;
+    void dfs(int u, int n, vector<vector<char>>& g, vector<bool>& col, vector<bool>& dg, vector<bool>& udg) {
+        if(u == n) {
+            res++;
+            return;
+        }
+        for(int i = 0; i < n; i++)
+        {
+            if(!col[i] && !dg[n+u-i] && !udg[u+i]) {
+                col[i] = dg[n+u-i] = udg[u+i] = true;
+                g[u][i] = 'Q';
+                dfs(u+1, n, g, col, dg, udg);
+                g[u][i] = '.';
+                col[i] = dg[n+u-i] = udg[u+i] = false;
+            }
+        }
+    }
+
+    int totalNQueens(int n) {
+        vector<vector<char>> g(n, vector<char>(n, '.'));
+        vector<bool> col(n, false);
+        vector<bool> dg(2*n, false);
+        vector<bool> udg(2*n, false);
+        dfs(0, n, g, col, dg, udg);
+        return res;
+    }
+};
+
 // AcWing打卡重做
 class Solution {
 public:
