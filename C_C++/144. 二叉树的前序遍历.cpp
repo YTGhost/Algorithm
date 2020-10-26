407d6b4c9af4c471023668cf4e7068b1d37f74bf
YTGhost <283304489@qq.com>
2020-10-27 00:27:50 +0800

    modify 144 2020/10/27

--- C_C++/144. 二叉树的前序遍历.cpp
+++ C_C++/144. 二叉树的前序遍历.cpp
@@ -7,6 +7,26 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+// 迭代
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+        stack<TreeNode*> s;
+        s.push(root);
+        while(!s.empty())
+        {
+            auto t = s.top();
+            s.pop();
+            res.emplace_back(t->val);
+            if(t->right) s.push(t->right);
+            if(t->left) s.push(t->left);
+        }
+        return res;
+    }
+};
+// 递归
 class Solution {
 public:
     vector<int> a;
