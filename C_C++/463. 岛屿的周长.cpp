45bbef7eb0b73f646fcd77c00d236a0501f8d16f
YTGhost <283304489@qq.com>
2020-10-30 20:46:00 +0800

    modify 463 2020/10/30

--- C_C++/463. 岛屿的周长.cpp
+++ C_C++/463. 岛屿的周长.cpp
@@ -1,3 +1,30 @@
+// 每日一题打卡
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int res = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        int dx[4] = {0, 1, 0, -1};
+        int dy[4] = {1, 0, -1, 0};
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+            {
+                if(grid[i][j]) {
+                    for(int k = 0; k < 4; k++)
+                    {
+                        int newX = j + dx[k];
+                        int newY = i + dy[k];
+                        if(newX < 0 || newY < 0 || newX >= m || newY >= n || !grid[newY][newX]) {
+                            res++;
+                        }
+                    }
+                }
+            }
+        return res;
+    }
+};
+
 class Solution {
 public:
     int ans;
