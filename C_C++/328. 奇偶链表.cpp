8620be984c645f8c08a8fa58d5a3558b463cfdac
YTGhost <283304489@qq.com>
2020-11-13 20:22:08 +0800

    modify 328 2020/11/13

--- C_C++/328. 奇偶链表.cpp
+++ C_C++/328. 奇偶链表.cpp
@@ -8,6 +8,27 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(!head) return head;
+        ListNode* odd = head;
+        ListNode* even = head->next;
+        ListNode* evenHead = even;
+
+        while(even && even->next)
+        {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
+        }
+        odd->next = evenHead;
+        return head;
+    }
+};
+
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
