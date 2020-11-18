58e1bd9b1624dfb077ae23ab9449eb60414d135f
YTGhost <283304489@qq.com>
2020-11-19 00:15:49 +0800

    add 283 2020/11/19

---
+++ C_C++/283. 移动零.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size(), left = 0, right = 0;
+        while (right < n) {
+            if (nums[right]) {
+                swap(nums[left], nums[right]);
+                left++;
+            }
+            right++;
+        }
+    }
+};
