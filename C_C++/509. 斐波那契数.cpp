6dd8868d51d0d338a31ceff563f0d2b5b9dc2249
YTGhost <283304489@qq.com>
2021-01-04 00:09:58 +0800

    modify 509 2020/01/04

--- C_C++/509. 斐波那契数.cpp
+++ C_C++/509. 斐波那契数.cpp
@@ -1,5 +1,18 @@
 class Solution {
 public:
+    int fib(int n) {
+        if(n < 2) return n;
+        int a = 1, b = 1;
+        for(int i = 2; i < n; i++) {
+            a += b;
+            b = a - b;
+        }
+        return a;
+    }
+};
+
+class Solution {
+public:
     int fib(int N) {
         if(N == 0 || N == 1) return N;
         int a = 0, b = 1, c = 0, i = 1;
