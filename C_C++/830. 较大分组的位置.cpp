4b83b1ce5c5d0f340c92473c0996fd5a63f3e0cd
YTGhost <283304489@qq.com>
2021-01-06 00:44:26 +0800

    add 830 2020/01/05

---
+++ C_C++/830. 较大分组的位置.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> largeGroupPositions(string s) {
+        vector<vector<int>> res;
+        int n = s.length();
+        int num = 1;
+        for(int i = 0; i < n; i++)
+        {
+            if(i == n-1 || s[i] != s[i+1]) {
+                if(num >= 3) res.push_back({i-num+1, i});
+                num = 1;
+            } else num++;
+        }
+        return res;
+    }
+};
