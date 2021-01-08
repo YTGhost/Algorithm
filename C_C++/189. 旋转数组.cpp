5298e795d327e48ce91f2d12d66be9e901bb07c9
YTGhost <283304489@qq.com>
2021-01-08 11:25:41 +0800

    add 189 2020/01/08

---
+++ C_C++/189. 旋转数组.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverse(vector<int>& nums, int start, int end) {
+        while(start < end) swap(nums[start++], nums[end--]);
+    }
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums, 0, nums.size()-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.size()-1);
+    }
+};
