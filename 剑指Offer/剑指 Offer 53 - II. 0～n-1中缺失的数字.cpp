5db47efad6eca556daa57aefd2fd9c4a63806a8d
YTGhost <283304489@qq.com>
2021-01-16 00:17:52 +0800

    modify

--- 剑指Offer/剑指 Offer 53 - II. 0～n-1中缺失的数字.cpp
+++ 剑指Offer/剑指 Offer 53 - II. 0～n-1中缺失的数字.cpp
@@ -1,8 +1,21 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int l = 0, r = nums.size();
+        while(l < r)
+        {
+            int mid = l + r >> 1;
+            if(nums[mid] != mid) r = mid;
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
         int l = 0, r = nums.size() - 1;
-        if(r == 0) return nums[0] ? 0 : 1; 
         while(l < r)
         {
             int mid = l + r >> 1;
