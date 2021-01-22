268777d375f50777ec92dc8204963d9fd0273b22
Deng Liang <283304489@qq.com>
2021-01-23 00:29:52 +0800

    modify 1319 2021/01/23

--- C_C++/1319. 连通网络的操作次数.cpp
+++ C_C++/1319. 连通网络的操作次数.cpp
@@ -1,3 +1,23 @@
+// 每日一题打卡
+class Solution {
+public:
+    int p[100010];
+    int find(int x) {
+        if(p[x] != x) p[x] = find(p[x]);
+        return p[x];
+    }
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(n-1 > connections.size()) return -1;
+        for(int i = 0; i < n; i++) p[i] = i;
+        for(auto connection : connections)
+            p[find(connection[0])] = find(p[connection[1]]);
+        int count = 0;
+        for(int i = 0; i < n; i++) 
+            if(p[i] == i) count++;
+        return count - 1;
+    }
+};
+
 class Solution {
 public:
     vector<int> p;
