ee086db6b041e8032cd7eb44a46fe1e80b515453
Deng Liang <283304489@qq.com>
2021-02-11 01:30:20 +0800

    add 703 2021/02/11

---
+++ C_C++/703. 数据流中的第 K 大元素.cpp
@@ -0,0 +1,22 @@
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> q;
+    int k;
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for(auto num : nums)
+            add(num);
+    }
+    
+    int add(int val) {
+        q.push(val);
+        if(q.size() > k) q.pop();
+        return q.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
