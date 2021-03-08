5a38596990d69b8f9931393eb7dbb23cc1f9487c
Deng Liang <283304489@qq.com>
2021-03-08 23:34:09 +0800

    add 118 2021/03/08

---
+++ C_C++/118. 杨辉三角.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        if(numRows == 0) return res;
+        res.emplace_back(vector<int>{1});
+        if(numRows == 1) return res;
+        res.emplace_back(vector<int>{1, 1});
+        if(numRows == 2) return res;
+        for(int i = 3; i <= numRows; i++)
+        {
+            vector<int> t(i);
+            t[0] = 1, t[i-1] = 1;
+            for(int j = 1; j < i-1; j++)
+                t[j] = res[i-2][j-1] + res[i-2][j];
+            res.emplace_back(t);
+        }
+        return res;
+    }
+};
