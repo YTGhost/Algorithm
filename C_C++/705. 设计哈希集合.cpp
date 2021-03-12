5936199e80f1b1d4692a33d521ea9e30b4fed4e9
YTGhost <283304489@qq.com>
2021-03-13 00:17:13 +0800

    add 705 2021/03/13

---
+++ C_C++/705. 设计哈希集合.cpp
@@ -0,0 +1,29 @@
+class MyHashSet {
+public:
+    /** Initialize your data structure here. */
+    int hash[1000001];
+    MyHashSet() {
+        memset(hash, 0, sizeof hash);
+    }
+    
+    void add(int key) {
+        hash[key] = 1;
+    }
+    
+    void remove(int key) {
+        hash[key] = 0;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    bool contains(int key) {
+        return hash[key];
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
