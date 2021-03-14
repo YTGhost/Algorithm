79d5fdc473f8ae74d37e016929b74d23929c76ff
YTGhost <283304489@qq.com>
2021-03-15 00:29:03 +0800

    modify 54 2021/03/15

--- C_C++/54. 螺旋矩阵.cpp
+++ C_C++/54. 螺旋矩阵.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<int> res;
+        vector<vector<bool>> st(m, vector<bool>(n, false));
+        int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
+        int x = 0, y = 0, d = 0;
+        for(int i = 0; i < m*n; i++)
+        {
+            res.emplace_back(matrix[x][y]);
+            st[x][y] = true;
+            int a = x + dx[d], b = y + dy[d];
+            if(a < 0 || a >= m || b < 0 || b >= n || st[a][b]) {
+                d = (d + 1) % 4;
+                a = x + dx[d], b = y + dy[d];
+            }
+            x = a, y = b;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
         vector<int> res;
         int n = matrix.size();
         if(!n) return res;
