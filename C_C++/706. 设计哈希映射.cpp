76372909e1b519123ca1eea4f52577725acc06ff
YTGhost <283304489@qq.com>
2021-03-14 23:04:05 +0800

    add 706 2021/03/14

---
+++ C_C++/706. 设计哈希映射.cpp
@@ -0,0 +1,31 @@
+class MyHashMap {
+public:
+    /** Initialize your data structure here. */
+    int hash[1000010];
+    MyHashMap() {
+        memset(hash, -1, sizeof hash);
+    }
+    
+    /** value will always be non-negative. */
+    void put(int key, int value) {
+        hash[key] = value;
+    }
+    
+    /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
+    int get(int key) {
+        return hash[key];
+    }
+    
+    /** Removes the mapping of the specified value key if this map contains a mapping for the key */
+    void remove(int key) {
+        hash[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
