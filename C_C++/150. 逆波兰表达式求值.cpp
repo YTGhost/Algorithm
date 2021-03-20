be27785dda1bd0d7d7d333e9bbf1a0dd146a1d18
YTGhost <283304489@qq.com>
2021-03-20 11:06:53 +0800

    modify 150 2021/03/20

--- C_C++/150. 逆波兰表达式求值.cpp
+++ C_C++/150. 逆波兰表达式求值.cpp
@@ -1,5 +1,43 @@
 class Solution {
 public:
+    bool isNumber(string& token) {
+        return token != "+" && token != "-" && token != "*" && token != "/";
+    }
+    int evalRPN(vector<string>& tokens) {
+        stack<int> stk;
+        int n = tokens.size();
+        for(int i = 0; i < n; i++)
+        {
+            string& token = tokens[i];
+            if(isNumber(token)) {
+                stk.push(atoi(token.c_str()));
+            } else {
+                int num1 = stk.top();
+                stk.pop();
+                int num2 = stk.top();
+                stk.pop();
+                switch (token[0]) {
+                    case '+':
+                        stk.push(num1 + num2);
+                        break;
+                    case '-':
+                        stk.push(num2 - num1);
+                        break;
+                    case '/':
+                        stk.push(num2 / num1);
+                        break;
+                    case '*':
+                        stk.push(num1 * num2);
+                        break;
+                }
+            }
+        }
+        return stk.top();
+    }
+};
+
+class Solution {
+public:
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
         for(int i = 0; i < tokens.size(); i++)
