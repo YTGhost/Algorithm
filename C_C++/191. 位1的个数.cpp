be6aa2201b699b9cdf007c38195066c6dbbffc88
YTGhost <283304489@qq.com>
2021-03-22 00:05:28 +0800

    modify 191 2021/03/22

--- C_C++/191. 位1的个数.cpp
+++ C_C++/191. 位1的个数.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        int res = 0;
+        while(n)
+        {
+            if(n & 1) res++;
+            n >>= 1;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
         int count = 0;
         while(n)
         {
