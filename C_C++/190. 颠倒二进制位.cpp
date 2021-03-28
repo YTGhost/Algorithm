5238d2921ecad5628266226ee3013fdee900b789
YTGhost <283304489@qq.com>
2021-03-29 01:10:46 +0800

    modify 190 2021/03/29

--- C_C++/190. 颠倒二进制位.cpp
+++ C_C++/190. 颠倒二进制位.cpp
@@ -1,3 +1,19 @@
+// 2021/03/29 每日一题
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        int i = 32;
+        while(i--)
+        {
+            res <<= 1;
+            res += n & 1;
+            n >>= 1;
+        }
+        return res;
+    }
+};
+
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
