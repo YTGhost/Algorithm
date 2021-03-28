d585d9f708b6a5e8b3acfb8af269b90932bb58a3
YTGhost <283304489@qq.com>
2021-03-28 12:43:18 +0800

    modify 173 2021/03/28

--- C_C++/173. 二叉搜索树迭代器.cpp
+++ C_C++/173. 二叉搜索树迭代器.cpp
@@ -4,6 +4,48 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class BSTIterator {
+public:
+    vector<int> res;
+    int index = 0;
+    BSTIterator(TreeNode* root) {
+        dfs(root);
+    }
+
+    void dfs(TreeNode* root) {
+        if(!root) return;
+        dfs(root->left);
+        res.emplace_back(root->val);
+        dfs(root->right);
+    }
+    
+    int next() {
+        return res[index++];
+    }
+    
+    bool hasNext() {
+        return index != res.size();
+    }
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
