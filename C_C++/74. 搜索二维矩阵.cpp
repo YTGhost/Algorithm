4f885a2916f36dc77ea57c032b017cdb27615894
YTGhost <283304489@qq.com>
2021-03-30 00:10:40 +0800

    modify 74 2021/03/30

--- C_C++/74. 搜索二维矩阵.cpp
+++ C_C++/74. 搜索二维矩阵.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int x = 0, y = m-1;
+        while(x < n && y >= 0)
+        {
+            if(matrix[x][y] == target) return true;
+            else if(matrix[x][y] > target) y--;
+            else x++;
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(matrix.empty()) return false;
         int i = 0, j = matrix[0].size() - 1;
         while(i < matrix.size() && j >= 0)
