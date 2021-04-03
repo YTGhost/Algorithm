840d64fa52986f39e5311524285c82d45bde0393
YTGhost <283304489@qq.com>
2021-04-03 11:10:21 +0800

    modify 1143 2021/04/03

--- C_C++/1143. 最长公共子序列.cpp
+++ C_C++/1143. 最长公共子序列.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> f(n+1, vector<int>(m+1, 0));
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= m; j++)
+                if(text1[i-1] == text2[j-1]) f[i][j] = max(f[i][j], f[i-1][j-1] + 1); 
+                else f[i][j] = max(f[i-1][j], f[i][j-1]);
+        return f[n][m];
+    }
+};
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
         int size1 = text1.length(), size2 = text2.length();
         vector<vector<int>> f(size1+1, vector<int>(size2+1, 0));
         for(int i = 1; i <= size1; i++)
