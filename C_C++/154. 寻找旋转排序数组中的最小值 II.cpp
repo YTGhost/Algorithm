100fe3c1e82cc506d26d844bfddb50dc46d63908
YTGhost <283304489@qq.com>
2021-04-09 08:55:59 +0800

    modify 154 2021/04/09

--- C_C++/154. 寻找旋转排序数组中的最小值 II.cpp
+++ C_C++/154. 寻找旋转排序数组中的最小值 II.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size()-1;
+        while(l < r)
+        {
+            int mid = l + r >> 1;
+            if(nums[mid] > nums[r]) l = mid + 1;
+            else if(nums[mid] < nums[r]) r = mid;
+            else r--;
+        }
+        return nums[l];
+    }
+};
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
         int n = nums.size() - 1;
         while(n > 0 && nums[n] == nums[0]) n--;
         if(nums[n] >= nums[0]) return nums[0];
