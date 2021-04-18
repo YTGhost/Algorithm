3dd5015741b0fff2d1f6d3cc03c5380fb21a39bf
YTGhost <283304489@qq.com>
2021-04-18 20:38:56 +0800

    modify 26 2021/04/18

--- C_C++/26. 删除排序数组中的重复项.cpp
+++ C_C++/26. 删除排序数组中的重复项.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 0) return 0;
+        int index = 1;
+        int pre = nums[0];
+        for(int i = 1; i < n; i++)
+        {
+            if(nums[i] != pre) {
+                nums[index++] = nums[i];
+                pre = nums[i];
+            }
+        }
+        return index;
+    }
+};
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
         int i = 0;
         for(int cur = 0, pre = 0; cur < nums.size(); i++)
         {
