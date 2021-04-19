71a45babc340d919ba6b0b8c997d42b09d837890
YTGhost <283304489@qq.com>
2021-04-19 14:15:12 +0800

    modify 27 2021/04/19

--- C_C++/27. 移除元素.cpp
+++ C_C++/27. 移除元素.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[i] != val) nums[index++] = nums[i];
+        return index;
+    }
+};
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
         int p1 = 0;
         int size = nums.size();
         for(int p2 = 0; p2 < size; p2++)
