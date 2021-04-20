bbc83ba7cdae758e38c13e02ca8ae33f5ee0f242
YTGhost <283304489@qq.com>
2021-04-20 21:56:19 +0800

    modify 28 2021/04/20

--- C_C++/28. 实现 strStr().cpp
+++ C_C++/28. 实现 strStr().cpp
@@ -1,3 +1,23 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.size(), m = needle.size();
+        for (int i = 0; i + m <= n; i++) {
+            bool flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
 // 暴力
 class Solution {
 public:
