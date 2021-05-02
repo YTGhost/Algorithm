97138777b3aa935bc7e2f93e8a8e034d9d85253b
YTGhost <283304489@qq.com>
2021-05-02 23:56:45 +0800

    add 554 2021/05/02

---
+++ C_C++/554. 砖墙.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int, int> cnt;
+        for (auto& widths : wall) {
+            int n = widths.size();
+            int sum = 0;
+            for (int i = 0; i < n - 1; i++) {
+                sum += widths[i];
+                cnt[sum]++;
+            }
+        }
+        int maxCnt = 0;
+        for (auto& [_, c] : cnt) {
+            maxCnt = max(maxCnt, c);
+        }
+        return wall.size() - maxCnt;
+    }
+};
