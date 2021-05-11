b6eeb1dc00dcdb82abf8d69ef2c7ee5b98df6208
YTGhost <283304489@qq.com>
2021-05-12 00:01:26 +0800

    modify 1022 2021/05/11

--- C_C++/1022. 从根到叶的二进制数之和.cpp
+++ C_C++/1022. 从根到叶的二进制数之和.cpp
@@ -1,11 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int sumRootToLeaf(TreeNode* root, int sub = 0) {
-        if (!root) return 0;
-        // 每增加一个节点，上一个节点就外前移一位。
-        // 而当前的值只可能是0 1 与十进制的0 1 是一个意思就直接累加
-        sub = (sub << 1) + root->val;
-        return root->left == root->right ? sub :
-            sumRootToLeaf(root->left, sub) + sumRootToLeaf(root->right, sub);
+    int res;
+    void dfs(TreeNode* root, int sum) {
+        if(!root) return;
+        sum <<= 1;
+        sum += root->val;
+        if(!root->left && !root->right) {
+            res += sum;
+            return;
+        }
+        dfs(root->left, sum);
+        dfs(root->right, sum);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        dfs(root, 0);
+        return res;
     }
 };
