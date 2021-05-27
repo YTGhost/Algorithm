47f4b56e048c9d2436d319d5c87480097305c6e3
YTGhost <283304489@qq.com>
2021-05-28 00:03:55 +0800

    add 461 2021/05/27

--- C_C++/461. 汉明距离.cpp
+++ C_C++/461. 汉明距离.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
+        int res = 0;
+        while(x && y) {
+            int a = x & 1, b = y & 1;
+            if(a != b) res++;
+            x >>= 1;
+            y >>= 1;
+        }
+        while(x) {
+            if(x&1) res++;
+            x >>= 1;
+        } 
+        while(y) {
+            if(y&1) res++;
+            y >>= 1;
+        } 
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
         int dist = 0;
         while(x && y)
         {
