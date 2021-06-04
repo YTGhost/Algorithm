e1c55f24d4c8a96e12b5936f8e7590c2396c82ad
YTGhost <283304489@qq.com>
2021-06-04 12:57:01 +0800

    modify 160 2021/06/04

--- C_C++/160.相交链表.cpp
+++ C_C++/160.相交链表.cpp
@@ -16,6 +16,19 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(!headA || !headB) return NULL;
+        ListNode* pa = headA, *pb = headB;
+        while(pa != pb) {
+            pa = !pa ? headB : pa->next;
+            pb = !pb ? headA : pb->next;
+        }
+        return pa;
+    }
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         unordered_set<ListNode*> a;
         while(headA != NULL){
             a.insert(headA);
