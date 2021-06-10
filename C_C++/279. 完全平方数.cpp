2dfcb23cc8f691768f76ef1697387e29dab36142
YTGhost <283304489@qq.com>
2021-06-11 00:28:07 +0800

    modify 279 2021/06/11

--- C_C++/279. 完全平方数.cpp
+++ C_C++/279. 完全平方数.cpp
@@ -1,3 +1,18 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> f(n + 1);
+        for(int i = 1; i <= n; i++) {
+            int t = INT_MAX;
+            for(int j = 1; j * j <= i; j++) {
+                t = min(t, f[i - j*j]);
+            }
+            f[i] = t + 1;
+        }
+        return f[n];
+    }
+};
+
 // 一维优化做法
 class Solution {
 public:
