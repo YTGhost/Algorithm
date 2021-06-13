a74324bcc1cb0fb64d0497d7ddb9ace994180f0e
YTGhost <283304489@qq.com>
2021-06-13 18:33:02 +0800

    modify 278 2021/06/13

--- C_C++/278. 第一个错误的版本.cpp
+++ C_C++/278. 第一个错误的版本.cpp
@@ -4,6 +4,22 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while(l < r) {
+            int mid = l + (r - l >> 1);
+            if(isBadVersion(mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
         long l = 1, r = n;
         while(l < r)
         {
