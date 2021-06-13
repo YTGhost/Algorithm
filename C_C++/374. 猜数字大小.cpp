01268f9f84ca65bea8075dc2494635439cac9b06
YTGhost <283304489@qq.com>
2021-06-14 02:04:57 +0800

    modify 374 2021/06/14

--- C_C++/374. 猜数字大小.cpp
+++ C_C++/374. 猜数字大小.cpp
@@ -10,6 +10,22 @@
 class Solution {
 public:
     int guessNumber(int n) {
+        int l = 1, r = n;
+        while(l < r) {
+            int mid = l + (r - l >> 1) + 1;
+            if(guess(mid) >= 0) {
+                l = mid;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return l;
+    }
+};
+
+class Solution {
+public:
+    int guessNumber(int n) {
         long l = 1, r = n;
         while(l < r)
         {
