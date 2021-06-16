d4de87d0748ec28b10dccb19ecf1082390682eb9
YTGhost <283304489@qq.com>
2021-06-16 23:10:25 +0800

    add 877 2021/06/16

---
+++ C_C++/877. 石子游戏.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        auto dp = vector<vector<int>>(n, vector<int>(n));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1] > 0;
+    }
+};
