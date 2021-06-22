4786bc87daa7702ff7d868d593d62a74750dff55
YTGhost <283304489@qq.com>
2021-06-22 13:40:38 +0800

    modify 剑指Offer 38 2021/06/22

--- 剑指Offer/剑指 Offer 38. 字符串的排列.cpp
+++ 剑指Offer/剑指 Offer 38. 字符串的排列.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     vector<string> res;
+    void dfs(string& str, int u, string cur, vector<int>& visited) {
+        int n = str.size();
+        if(u == n) {
+            res.push_back(cur);
+            return;
+        }
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && !visited[i-1] && str[i] == str[i - 1]) continue;
+            if(!visited[i]) {
+                visited[i] = 1;
+                dfs(str, u + 1, cur + str[i], visited);
+                visited[i] = 0;
+            }
+        }
+    }
+    vector<string> permutation(string s) {
+        int n = s.size();
+        vector<int> visited(n);
+        sort(s.begin(), s.end());
+        dfs(s, 0, "", visited);
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<string> res;
     string path = "11111111";
 
     void dfs(string& s, int u, int start, int state) {
