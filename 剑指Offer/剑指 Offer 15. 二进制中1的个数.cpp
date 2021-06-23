89fc7947e3eca510799bd95e19274ccffe5ba746
YTGhost <283304489@qq.com>
2021-06-23 12:18:48 +0800

    modify 剑指Offer 15 2021/06/23

--- 剑指Offer/剑指 Offer 15. 二进制中1的个数.cpp
+++ 剑指Offer/剑指 Offer 15. 二进制中1的个数.cpp
@@ -2,6 +2,18 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int res = 0;
+        while(n) {
+            if(n & 1) res++;
+            n >>= 1;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int res = 0;
         while(n)
         {
             res += n&1;
