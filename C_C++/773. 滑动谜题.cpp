1a5fc0232b290b55a7299003e5437ee3e1146359
YTGhost <283304489@qq.com>
2021-06-26 21:16:21 +0800

    modify 773 2021/06/26

--- C_C++/773. 滑动谜题.cpp
+++ C_C++/773. 滑动谜题.cpp
@@ -1,5 +1,44 @@
 class Solution {
 public:
+    int bfs(string start) {
+        unordered_map<string, int> d;
+        queue<string> q;
+        string end = "123450";
+        d[start] = 0;
+        q.push(start);
+        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        while(!q.empty()) {
+            string t = q.front();
+            int distance = d[t];
+            q.pop();
+            if(t == end) return d[t];
+            int index = t.find('0');
+            int a = index / 3, b = index % 3;
+            for(int i = 0; i < 4; i++) {
+                int newA = a + dx[i], newB = b + dy[i];
+                if(newA >= 0 && newA < 2 && newB >= 0 && newB < 3) {
+                    swap(t[index], t[3*newA + newB]);
+                    if(!d.count(t)) {
+                        d[t] = distance + 1;
+                        q.push(t);
+                    }
+                    swap(t[index], t[3*newA + newB]);
+                }
+            }
+        }
+        return -1;
+    }
+    int slidingPuzzle(vector<vector<int>>& board) {
+        string start;
+        for(int i = 0; i < 2; i++)
+            for(int j = 0; j < 3; j++)
+                start += to_string(board[i][j]);
+        return bfs(start);
+    }
+};
+
+class Solution {
+public:
     int bfs(string start){
         unordered_map<string, int> d;
         queue<string> q;
