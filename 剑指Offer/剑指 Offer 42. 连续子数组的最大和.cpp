2645a9eadcbbfd31b5fca638d7475e8b2fc6b9eb
YTGhost <283304489@qq.com>
2021-07-17 01:55:24 +0800

    modify 剑指42

--- 剑指Offer/剑指 Offer 42. 连续子数组的最大和.cpp
+++ 剑指Offer/剑指 Offer 42. 连续子数组的最大和.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
+        int sum = 0;
+        int res = INT_MIN;
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            res = max(res, sum);
+            if(sum < 0) sum = 0;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
         int sum = 0, res = INT_MIN;
         for(auto num : nums)
         {
