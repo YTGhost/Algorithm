f7ee46acff134f5c56be482d758170d644bd3a10
YTGhost <283304489@qq.com>
2021-07-21 19:57:58 +0800

    add 剑指52 2021/07/21

---
+++ 剑指Offer/剑指 Offer 52. 两个链表的第一个公共节点.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB == NULL) return NULL;
+        ListNode* p1 = headA;
+        ListNode* p2 = headB;
+        while(p1 != p2) {
+            p1 = p1 == NULL ? headB : p1->next;
+            p2 = p2 == NULL ? headA : p2->next;
+        }
+        return p1;
+    }
+};
