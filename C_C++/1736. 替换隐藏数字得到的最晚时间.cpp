751f703fd8348b2f93de09e65b414712f968591a
YTGhost <283304489@qq.com>
2021-07-24 00:29:01 +0800

    add 1736 2021/07/24

---
+++ C_C++/1736. 替换隐藏数字得到的最晚时间.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string maximumTime(string time) {
+        if(time[0] == '?') {
+            if(time[1] > '3' && time[1] <= '9') {
+                time[0] = '1';
+            } else {
+                time[0] = '2';
+            }
+        }
+        if(time[1] == '?') {
+            if(time[0] == '1' || time[0] == '0') {
+                time[1] = '9';
+            } else {
+                time[1] = '3';
+            }
+        }
+        if(time[3] == '?') time[3] = '5';
+        if(time[4] == '?') time[4] = '9';
+        return time;
+    }
+};
