bbca2964285a07c34483760c3e7a38226b1f0180
YTGhost <283304489@qq.com>
2021-08-03 15:33:10 +0800

    add 剑指43 2021/08/03

---
+++ 剑指Offer/剑指 Offer 43. 1～n 整数中 1 出现的次数.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int calc(int n) {
+        vector<int> nums;
+        int res = 0;
+        while(n) nums.push_back(n % 10), n /= 10;
+
+        for(int i = nums.size()-1; i >= 0; i--) {
+            int l = 0, r = 0, power = 1;
+            for(int j = nums.size()-1; j > i; j--) l = l * 10 + nums[j];
+            for(int j = i-1; j >= 0; j--) {
+                r = r * 10 + nums[j];
+                power *= 10;
+            }
+
+            if(nums[i] == 0) {
+                res += l * power;
+            } else if(nums[i] == 1) {
+                res += l * power + r + 1;
+            } else {
+                res += (l + 1) * power;
+            }
+        }
+        return res;
+    }
+    int countDigitOne(int n) {
+        return calc(n);
+    }
+};
