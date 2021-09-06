9b3462a2d13c8e3add6b1c332f045a7f3ceb393b
YTGhost <283304489@qq.com>
2021-09-06 09:58:25 +0800

    modify 704 2021/09/06

--- C_C++/704. 二分查找.cpp
+++ C_C++/704. 二分查找.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while(l < r) {
+            int mid = l + r + 1 >> 1;
+            if(nums[mid] > target) r = mid - 1;
+            else l = mid;
+        }
+        return nums[l] == target ? l : -1;
+    }
+};
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
         int l = 0, r = nums.size()-1;
         while(l < r)
         {
