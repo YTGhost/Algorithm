522f5594424d405eb666dc11c8799384b9300f3e
YTGhost <283304489@qq.com>
2021-10-10 12:07:52 +0800

    add 441 2021/10/10

--- C_C++/441. 排列硬币.cpp
+++ C_C++/441. 排列硬币.cpp
@@ -4,3 +4,16 @@ public:
         return (-1 + sqrt(1 + 8 * (long)n)) / 2;
     }
 };
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int res = 0;
+        int idx = 1;
+        while(n - idx >= 0) {
+            n -= idx++;
+            res++;
+        }
+        return res;
+    }
+};
