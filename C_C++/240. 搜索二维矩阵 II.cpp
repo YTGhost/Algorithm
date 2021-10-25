f5e20eda2fd07d4896d5a52b84b920697d9c297e
YTGhost <283304489@qq.com>
2021-10-25 19:24:09 +0800

    modify 240 2021/10/25

--- C_C++/240. 搜索二维矩阵 II.cpp
+++ C_C++/240. 搜索二维矩阵 II.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int x = matrix.size() - 1, y = 0;
+        while(x >= 0 && y < matrix[0].size()) {
+            if(matrix[x][y] > target) x--;
+            else if(matrix[x][y] < target) y++;
+            else return true;
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
         if(matrix.empty()) return false;
         int i = 0, j = matrix[0].size() - 1;
         while(i < matrix.size() && j >= 0)
