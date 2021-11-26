8c28b3994a30a5741e0b313e39404df44dc6e431
YTGhost <283304489@qq.com>
2021-11-27 00:01:59 +0800

    modify 700 2021/11/27

--- C_C++/700. 二叉搜索树中的搜索.cpp
+++ C_C++/700. 二叉搜索树中的搜索.cpp
@@ -9,6 +9,22 @@
  */
 class Solution {
 public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        while(root) {
+            if(root->val > val) {
+                root = root->left;
+            } else if(root->val < val) {
+                root = root->right;
+            } else {
+                return root;
+            }
+        }
+        return NULL;
+    }
+};
+
+class Solution {
+public:
     TreeNode* res;
     TreeNode* searchBST(TreeNode* root, int val) {
         if(!root) return NULL;
