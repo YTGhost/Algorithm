8486960b26f4e49fdc6cf93397437b6838d4473f
YTGhost <283304489@qq.com>
2021-12-04 19:22:03 +0800

    modify 383 2021/12/04

--- C_C++/383. 赎金信.cpp
+++ C_C++/383. 赎金信.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> m;
+        for(auto c : magazine) {
+            m[c]++;
+        }
+        for(auto c : ransomNote) {
+            if(m[c]) m[c]--;
+            else return false;
+        }
+        return true;
+    }
+};
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
         int sign[26];
         memset(sign, 0, sizeof(sign));
         for(int i = 0; i < magazine.length(); i++) sign[magazine[i] - 'a']++;
