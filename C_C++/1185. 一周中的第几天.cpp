e6eeacf8f9ba249ba369f12e95a5ed3df77baf71
YTGhost <283304489@qq.com>
2022-01-03 12:48:27 +0800

    add 1185 2022/01/03

---
+++ C_C++/1185. 一周中的第几天.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string dayOfTheWeek(int day, int month, int year) {
+        vector<string> ss = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        vector<int> nums = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int res = 4;
+        for(int i = 1971; i < year; i++) {
+            bool t = (i % 4 == 0 && i % 100 != 0) || i % 400 == 0;
+            res += t ? 366 : 365;
+        }
+        for(int i = 1; i < month; i++) {
+            res += nums[i - 1];
+            if(i == 2 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) res++;
+        }
+        res += day;
+        return ss[res % 7];
+    }
+};
