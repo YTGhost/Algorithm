90de9a69dab7f20ada6f0e04f3c20266b056cef1
YTGhost <283304489@qq.com>
2022-02-06 11:23:19 +0800

    modify 1748 2022/02/06

--- C_C++/1748. 唯一元素的和.cpp
+++ C_C++/1748. 唯一元素的和.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+    int hash[105];
+    int sumOfUnique(vector<int>& nums) {
+        int res = 0;
+        for(auto num : nums) {
+            hash[num]++;
+        }
+        for(int i = 1; i <= 100; i++) {
+            if(hash[i] == 1) {
+                res += i;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
     int hash[101];
     int sumOfUnique(vector<int>& nums) {
         for(auto num : nums) {
