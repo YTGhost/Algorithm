3937d526b5d9969fa17e0e3bcc066fe8cbcb67eb
YTGhost <283304489@qq.com>
2022-04-03 11:31:55 +0800

    modify 744 2022/04/03

--- C_C++/744. 寻找比目标字母大的最小字母.cpp
+++ C_C++/744. 寻找比目标字母大的最小字母.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = 0, r = letters.size();
+        int n = letters.size();
+        while(l < r) {
+            int mid = l + r >> 1;
+            if(letters[mid] > target) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return letters[l % n];
+    }
+};
+
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
         int l = 0, r = letters.size()-1;
         while(l < r)
         {
