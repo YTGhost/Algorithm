23db09f8d4955320752db16b327397f0a080850e
YTGhost <283304489@qq.com>
2022-06-08 14:20:53 +0800

    modify 1037 2022/06/08

--- C_C++/1037. 有效的回旋镖.cpp
+++ C_C++/1037. 有效的回旋镖.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+    bool isBoomerang(vector<vector<int>>& points) {
+        int x1 = points[1][0] - points[0][0];
+        int y1 = points[1][1] - points[0][1];
+        int x2 = points[2][0] - points[0][0];
+        int y2 = points[2][1] - points[0][1];
+        return y1 * x2 != y2 * x1;
+    }
+};
+
+class Solution {
+public:
     bool isBoomerang(vector<vector<int>>& p) {
         return (p[0][0] * (p[1][1] - p[2][1]) + p[1][0] * (p[2][1] - p[0][1]) + p[2][0] * (p[0][1] - p[1][1])) != 0;
     }
