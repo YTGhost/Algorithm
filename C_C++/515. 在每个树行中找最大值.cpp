ec07a5dd62770cc9344cde1a59a25299c3b7ab7a
YTGhost <283304489@qq.com>
2022-06-24 00:15:01 +0800

    modify 515 2022/06/24

--- C_C++/515. 在每个树行中找最大值.cpp
+++ C_C++/515. 在每个树行中找最大值.cpp
@@ -9,6 +9,29 @@
  */
 class Solution {
 public:
+    vector<int> largestValues(TreeNode* root) {
+        vector<int> res;
+        if(!root) return res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            int maxNum = INT_MIN;
+            for(int i = 0; i < n; i++) {
+                auto t = q.front();
+                q.pop();
+                if(t->left) q.push(t->left);
+                if(t->right) q.push(t->right);
+                maxNum = max(maxNum, t->val);
+            }
+            res.push_back(maxNum);
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
     vector<int> ans;
     queue<TreeNode*> q;
     vector<int> largestValues(TreeNode* root) {
