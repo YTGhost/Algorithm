8f7f97322ca638553ae5eae258a73f387343758c
YTGhost <283304489@qq.com>
2022-07-04 08:53:54 +0800

    modify 1200 2022/07/04

--- C_C++/1200. 最小绝对差.cpp
+++ C_C++/1200. 最小绝对差.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        vector<vector<int>> res;
+        int minVal = INT_MAX;
+        int n = arr.size();
+        for(int i = 1; i < n; i++) {
+            int a = arr[i - 1], b = arr[i];
+            int diff = b - a;
+            if(diff < minVal) {
+                minVal = b - a;
+                res.clear();
+                res.push_back({a, b});
+            } else if(diff == minVal) {
+                res.push_back({a, b});
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
         vector<vector<int>> res;
         int t = 0x3f3f3f3f;
         sort(arr.begin(), arr.end());
