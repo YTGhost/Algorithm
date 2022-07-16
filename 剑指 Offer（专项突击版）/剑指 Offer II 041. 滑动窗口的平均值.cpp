43bf92d155f28e95ed5b5f0d680c3b1d059e51a0
YTGhost <283304489@qq.com>
2022-07-16 09:57:13 +0800

    modify 剑指专项41 2022/07/16

--- 剑指 Offer（专项突击版）/剑指 Offer II 041. 滑动窗口的平均值.cpp
+++ 剑指 Offer（专项突击版）/剑指 Offer II 041. 滑动窗口的平均值.cpp
@@ -1,6 +1,38 @@
 class MovingAverage {
 public:
     /** Initialize your data structure here. */
+    int n;
+    int sum;
+    queue<int> q;
+    MovingAverage(int size) {
+        n = size;
+        sum = 0;
+    }
+    
+    double next(int val) {
+        if(q.size() < n) {
+            sum += val;
+            q.push(val);
+        } else {
+            int t = q.front();
+            sum -= t;
+            q.pop();
+            q.push(val);
+            sum += val;
+        }
+        return sum * 1.0 / q.size();
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
+
+class MovingAverage {
+public:
+    /** Initialize your data structure here. */
     vector<int> window;
     double sum;
     int idx, size;
