c34b2a6f2d8dbd567fe0bf63b4b9861afe0363d0
YTGhost <283304489@qq.com>
2022-08-06 10:25:19 +0800

    add 1408 2022/08/06

---
+++ C_C++/1408. 数组中的字符串匹配.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> stringMatching(vector<string>& words) {
+        vector<string> ret;
+        for (int i = 0; i < words.size(); i++) {
+            for (int j = 0; j < words.size(); j++) {
+                cout << words[i] << " " << words[j] << endl;
+                if (i != j && words[j].find(words[i]) != string::npos) {
+                    ret.push_back(words[i]);
+                    break;
+                }
+            }
+        }
+        return ret;
+    }
+};
