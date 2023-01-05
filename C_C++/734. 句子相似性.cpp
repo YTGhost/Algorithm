f2ea152cbeffb3a1baad211a1378edf6a7bf2810
HIHIA <283304489@qq.com>
2023-01-05 12:18:25 +0800

    add 734 2023/01/05

---
+++ C_C++/734. 句子相似性.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool areSentencesSimilar(vector<string>& sentence1, vector<string>& sentence2, vector<vector<string>>& similarPairs) {
+        if(sentence1.size() != sentence2.size()) {
+            return false;
+        }
+        unordered_set<string> s;
+        for(int i = 0; i < similarPairs.size(); i++) {
+            s.insert(similarPairs[i][0] + "#" + similarPairs[i][1]);
+        }
+        for(int i = 0; i < sentence1.size(); i++) {
+            if(sentence1[i] == sentence2[i]) continue;
+            if(!s.count(sentence1[i] + "#" + sentence2[i]) && !s.count(sentence2[i] + "#" + sentence1[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
