48083a99e728b2837944e147a9e27477ac0672f0
HIHIA <283304489@qq.com>
2023-01-07 13:53:35 +0800

    modify 1658 2023/01/07

--- C_C++/1658. 将 x 减到 0 的最小操作数.cpp
+++ C_C++/1658. 将 x 减到 0 的最小操作数.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
+        int n = nums.size();
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum < x) {
+            return -1;
+        }
+        int right = 0, leftsum = 0, rightsum = sum, res = n + 1;
+        for(int left = -1; left < n; left++) {
+            if(left != -1) {
+                leftsum += nums[left];
+            }
+            while(right < n && leftsum + rightsum > x) {
+                rightsum -= nums[right];
+                right++;
+            }
+            if(leftsum + rightsum == x) {
+                res = min(res, left + 1 + n - right);
+            }
+        }
+        return res > n ? -1 : res;
+    }
+};
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
         int sum = 0, n = nums.size();
         for(int i = 0; i < n; i++) sum += nums[i];
         sum -= x;
