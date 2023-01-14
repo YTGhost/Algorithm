8ce4549198fa1ebb3baf692b7f638fc02f6cf31d
HIHIA <283304489@qq.com>
2023-01-14 23:00:00 +0800

    add 2331 2023/01/14

---
+++ C_C++/2331. 计算布尔二叉树的值.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool evaluateTree(TreeNode* root) {
+        if(!root->left || !root->right) return root->val;
+        bool left = evaluateTree(root->left);
+        bool right = evaluateTree(root->right);
+        return root->val == 2 ? left || right : left && right;
+    }
+};
