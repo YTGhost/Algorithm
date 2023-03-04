98f70f2a18fb15d9e469ec493a2c35be12b8201e
HIHIA <283304489@qq.com>
2023-03-04 19:41:03 +0800

    add 1572 2023/03/04

---
+++ C_C++/1572. 矩阵对角线元素的和.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size(), sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += mat[i][i] + mat[i][n-i-1];
+        }
+        if(n % 2 != 0) {
+            sum -= mat[n/2][n/2];
+        }
+        return sum;
+    }
+};
