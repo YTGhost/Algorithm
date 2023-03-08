46df439e729e7131ea3d4db49454fac86acf13d3
HIHIA <283304489@qq.com>
2023-03-08 09:14:49 +0800

    modify 剑指47 2023/03/08

--- 剑指Offer/剑指 Offer 47. 礼物的最大价值.cpp
+++ 剑指Offer/剑指 Offer 47. 礼物的最大价值.cpp
@@ -1,3 +1,22 @@
+class Solution {
+public:
+    int maxValue(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        for(int i = 1; i < n; i++) {
+            grid[0][i] += grid[0][i-1];
+        }
+        for(int i = 1; i < m; i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                grid[i][j] += max(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+};
+
 // 一维 
 class Solution {
 public:
