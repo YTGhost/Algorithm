74812957aa8daf75217b3221524db10269d7fd93
HIHIA <283304489@qq.com>
2023-03-14 20:48:40 +0800

    add 1605 2023/03/14

---
+++ C_C++/1605. 给定行和列的和求可行矩阵.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+        int n = rowSum.size(), m = colSum.size();
+        vector<vector<int>> res(n, vector<int>(m));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                res[i][j] = min(rowSum[i], colSum[j]);
+                rowSum[i] -= res[i][j];
+                colSum[j] -= res[i][j];
+            }
+        }
+        return res;
+    }
+};
