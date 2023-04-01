dc701c331be391b6284833342c8e5d463c70e143
Liang Deng <283304489@qq.com>
2023-04-01 14:59:01 +0800

    add 831 2023/04/01

---
+++ C_C++/831. 隐藏个人信息.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string maskPII(string s) {
+        vector<string> country = {"", "+*-", "+**-", "+***-"};
+        int at = s.find("@");
+        if (at != -1) {
+            transform(s.begin(), s.end(), s.begin(), ::tolower);
+            return s.substr(0, 1) + "*****" + s.substr(at - 1);
+        }
+        s = regex_replace(s, regex("[^0-9]"), "");
+        return country[s.size() - 10] + "***-***-" + s.substr(s.size() - 4);
+    }
+};
