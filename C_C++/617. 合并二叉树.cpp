ea51a8fc5924a2b66bf1b78a1f83d9bd7bb98e76
Liang Deng <283304489@qq.com>
2023-08-14 23:38:51 +0800

    modify 617 2023/08/14

--- C_C++/617. 合并二叉树.cpp
+++ C_C++/617. 合并二叉树.cpp
@@ -18,3 +18,14 @@ public:
         return t1;
     }
 };
+
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        if(!root1) return root2;
+        if(!root2) return root1;
+        return new TreeNode(root1->val + root2->val, 
+        mergeTrees(root1->left, root2->left), 
+        mergeTrees(root1->right, root2->right));
+    }
+};
