f9b0df57a4513403c8b920570a4454bd266229a2
Liang Deng <283304489@qq.com>
2023-08-27 18:21:07 +0800

    modify 56 2023/08/27

--- C_C++/56. 合并区间.cpp
+++ C_C++/56. 合并区间.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> res;
+        for(int i = 0; i < intervals.size(); i++) {
+            int l = intervals[i][0], r = intervals[i][1];
+            if(!res.size() || res.back()[1] < l) {
+                res.push_back({l, r});
+            } else {
+                res.back()[1] = max(res.back()[1], r);
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
         if(intervals.size() == 1 || intervals.size() == 0) return intervals;
         vector<vector<int>> ans;
         sort(intervals.begin(), intervals.end());
