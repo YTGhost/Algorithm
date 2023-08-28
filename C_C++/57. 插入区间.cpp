77eb850dfc39a6f1f17adb5a7ebcc8bd8221fcf7
Liang Deng <283304489@qq.com>
2023-08-28 23:37:34 +0800

    modify 57 2023/08/28

--- C_C++/57. 插入区间.cpp
+++ C_C++/57. 插入区间.cpp
@@ -1,3 +1,5 @@
+
+
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
@@ -7,7 +9,32 @@ public:
         for(const auto &interval : intervals) {
 
             if(interval[0] > right) {
+                if(!flag) {class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> res;
+        int l = newInterval[0], r = newInterval[1];
+        bool flag = false;
+        for(const auto &interval : intervals) {
+            if(interval[1] < l) {
+                res.push_back(interval);
+            } else if(interval[0] > r) {
                 if(!flag) {
+                    res.push_back({l, r});
+                    flag = true;
+                }
+                res.push_back(interval);
+            } else {
+                l = min(l, interval[0]);
+                r = max(r, interval[1]);
+            }
+        }
+        if(!flag) {
+            res.push_back({l, r});
+        }
+        return res;
+    }
+};
                     res.push_back({left, right});
                     flag = true;
                 }
