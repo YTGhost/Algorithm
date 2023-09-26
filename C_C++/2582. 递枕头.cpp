5f19aec6cdaee6beb23ce684efc06ab8e97b77e1
Liang Deng <283304489@qq.com>
2023-09-26 23:23:45 +0800

    add 2582 2023/09/26

--- C_C++/2582. 递枕头.cpp
+++ C_C++/2582. 递枕头.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
+        int t = (n - 1) * 2;
+        time = time % t;
+        int res;
+        if(time > n - 1) {
+            res = n - (time - n + 1);
+        } else {
+            res = time + 1;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int passThePillow(int n, int time) {
         int a = time / (n - 1);
         int b = time % (n - 1);
         int res;
