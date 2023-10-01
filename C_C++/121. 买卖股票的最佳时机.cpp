015aba524ae25a2c47e5da20058a2041bd4d9f5e
Liang Deng <283304489@qq.com>
2023-10-01 11:57:05 +0800

    modify 121 2023/10/01

--- C_C++/121. 买卖股票的最佳时机.cpp
+++ C_C++/121. 买卖股票的最佳时机.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int cost = INT_MAX, profit = 0;
+        for(int i = 0; i < n; i++) {
+            cost = min(cost, prices[i]);
+            profit = max(profit, prices[i] - cost);
+        }
+        return profit;
+    }
+};
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
         int minPrice = INT_MAX;
         int maxProfit = 0;
         for(int i = 0; i < prices.size(); i++)
