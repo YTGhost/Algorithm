3b90405458df375eac5ee168d044bdcce7b345d0
Liang Deng <283304489@qq.com>
2023-10-14 01:39:12 +0800

    modify 136 2023/10/14

--- C_C++/136. 只出现一次的数字.cpp
+++ C_C++/136. 只出现一次的数字.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(auto num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
         int ans = 0;
         for(int i = 0; i < nums.size(); i++) ans ^= nums[i];
         return ans;
