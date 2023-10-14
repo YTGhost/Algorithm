2380b339d4804f580194982d6a603c81075cf696
Liang Deng <283304489@qq.com>
2023-10-15 00:42:01 +0800

    add 137 2023/10/15

--- C_C++/137. 只出现一次的数字 II.cpp
+++ C_C++/137. 只出现一次的数字 II.cpp
@@ -2,6 +2,23 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int res = 0;
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            for(auto num : nums) {
+                sum += ((num >> i) & 1);
+            }
+            if(sum % 3) {
+                res |= (1 << i);
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
         for(int i = 0; i < 32; i++)
         {
             int cnt = 0;
