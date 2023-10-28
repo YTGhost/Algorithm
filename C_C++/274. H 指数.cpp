d590c7819ee3c32198667e9bf7bb6d1e24056d45
Liang Deng <283304489@qq.com>
2023-10-29 01:08:57 +0800

    modify 274 2023/10/29

--- C_C++/274. H 指数.cpp
+++ C_C++/274. H 指数.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        int res = 0;
+        sort(citations.begin(), citations.end());
+        for(int i = n - 1; i >= 0; i--) {
+            if(citations[i] > res) {
+                res++;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
         sort(citations.begin(), citations.end());
         int n = citations.size();
         int h = 0;
