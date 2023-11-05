24acc71f2dd07fd534ba71350fa51396b1ebf4f4
Liang Deng <283304489@qq.com>
2023-11-05 09:57:04 +0800

    add 187 2023/11/05

--- C_C++/187. 重复的DNA序列.cpp
+++ C_C++/187. 重复的DNA序列.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
         unordered_map<string, int> m;
+        int n = s.size();
+        for(int i = 0; i < n; i++) {
+            string str = s.substr(i, 10);
+            m[str]++;
+        }
+        vector<string> res;
+        for(auto& [k, v] : m) {
+            if(v > 1) {
+                res.push_back(k);
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string, int> m;
         vector<string> res;
         if(s.length() <= 10) return res;
         for(int i = 0; i <= s.length() - 10; i++) {
