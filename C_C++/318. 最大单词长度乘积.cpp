32d95330ffa4c88d5a5b4a0a5ed97774c1b4887b
Liang Deng <283304489@qq.com>
2023-11-06 12:13:51 +0800

    add 318 2023/11/06

--- C_C++/318. 最大单词长度乘积.cpp
+++ C_C++/318. 最大单词长度乘积.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int maxProduct(vector<string>& words) {
+        int res = 0, n = words.size();
+        vector<int> mask(n);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < words[i].size(); j++) {
+                mask[i] |= 1 << (words[i][j] - 'a');
+            }
+        }
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if((mask[i] & mask[j]) == 0) {
+                    int len = words[i].size() * words[j].size();
+                    res = max(res, len);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
         int len = words.size();
         vector<int> temp(len);
         for(int i = 0; i < len; i++) {
