77d3988d9147cc67270cb0def90ee1da2142efa7
Liang Deng <283304489@qq.com>
2023-11-20 00:58:47 +0800

    modify 53 2023/11/20

--- C_C++/53. 最大子序和.cpp
+++ C_C++/53. 最大子序和.cpp
@@ -1,3 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int res = INT_MIN, sum = 0, n = nums.size();
+        for(int i = 0; i < n; i++) {
+            sum += nums[i];
+            res = max(res, sum);
+            if(sum < 0) {
+                sum = 0;
+            }
+        }
+        return res;
+    }
+};
+
 // AcWing打卡重做
 class Solution {
 public:
