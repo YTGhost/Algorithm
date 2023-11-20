8a645b07af8fd5cbb9fc0171b6ea3f8362aab8ea
Liang Deng <283304489@qq.com>
2023-11-20 21:53:50 +0800

    add 686 2023/11/20

--- C_C++/686. 重复叠加字符串匹配.cpp
+++ C_C++/686. 重复叠加字符串匹配.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     int repeatedStringMatch(string a, string b) {
+        int res = -1;
+        for(int i = 0; i < a.size(); i++) {
+            int l = i, r = 0;
+            int cnt = 1;
+            while(r < b.size() && a[l % a.size()] == b[r]) {
+                l++;
+                r++;
+                if(l == a.size() && r != b.size()) {
+                    cnt++;
+                    l %= a.size();
+                }
+            }
+            if(r >= b.size()) {
+                res = max(res, cnt);
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
         int n = a.length(), m = b.length();
         int res = 0;
         string temp = "";
