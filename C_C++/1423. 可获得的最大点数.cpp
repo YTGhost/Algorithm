c3b789afe9a3ac119abe636155e85bcfbf77e1ac
Liang Deng <283304489@qq.com>
2023-12-03 11:05:51 +0800

    modify 1423 2023/12/03

--- C_C++/1423. 可获得的最大点数.cpp
+++ C_C++/1423. 可获得的最大点数.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int len = n - k;
+        int totalSum = accumulate(cardPoints.begin(), cardPoints.end(), 0);
+        int sum = 0;
+        for(int i = 0; i < len; i++) {
+            sum += cardPoints[i];
+        }
+        int res = sum;
+        int l = 1, r = n - k;
+        while(r < n) {
+            sum = sum - cardPoints[l - 1] + cardPoints[r];
+            res = min(res, sum);
+            l++;
+            r++;
+        }
+        return totalSum - res;
+    }
+};
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
         int sum = 0, res = INT_MAX, n = cardPoints.size();
         for(int i = 0; i < n-k; i++) 
             sum += cardPoints[i];
