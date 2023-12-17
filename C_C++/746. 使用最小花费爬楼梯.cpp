0b3cc66da1098887b15c066c38a0a89086d69fec
Liang Deng <283304489@qq.com>
2023-12-17 16:54:06 +0800

    modify 746 2023/12/17

--- C_C++/746. 使用最小花费爬楼梯.cpp
+++ C_C++/746. 使用最小花费爬楼梯.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int> f(n + 1);
+        for(int i = 2; i <= n; i++) {
+            f[i] = min(f[i - 1] + cost[i - 1], f[i - 2] + cost[i - 2]);
+        }
+        return f[n];
+    }
+};
+
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
         int pre = 0;
         int cur = 0;
 
