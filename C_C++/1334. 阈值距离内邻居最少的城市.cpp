3eb4e90d8925ad6cbc3b226f8bb3a05590c2ce68
Liang Deng <283304489@qq.com>
2023-12-24 22:03:34 +0800

    add 1334 2023/12/24

---
+++ C_C++/1334. 阈值距离内邻居最少的城市.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> w(n, vector<int>(n, INT_MAX / 2));
+        for(auto &e : edges) {
+            int x = e[0], y = e[1], wt = e[2];
+            w[x][y] = w[y][x] = wt;
+        }
+
+        vector<vector<vector<int>>> f(n + 1, vector<vector<int>>(n, vector<int>(n)));
+        f[0] = w;
+        for(int k = 0; k < n; k++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    f[k + 1][i][j] = min(f[k][i][j], f[k][i][k] + f[k][k][j]);
+                }
+            }
+        }
+        int res = 0;
+        int min_cnt = n;
+        for(int i = 0; i < n; i++) {
+            int cnt = 0;
+            for(int j = 0; j < n; j++) {
+                if(i != j && f[n][i][j] <= distanceThreshold) {
+                    cnt++;
+                }
+            }
+            if(cnt <= min_cnt) {
+                min_cnt = cnt;
+                res = i;
+            }
+        }
+        return res;
+    }
+};
