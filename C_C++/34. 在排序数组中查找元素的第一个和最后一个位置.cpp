5f6cef15d5c7e379206f3d921ea47d13733e4616
Liang Deng <283304489@qq.com>
2023-12-27 13:25:45 +0800

    modify 34 2023/12/27

--- C_C++/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
+++ C_C++/34. 在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -1,5 +1,29 @@
 class Solution {
 public:
+    int lower_bound(vector<int>& nums, int target) {
+        int l = 0, r = nums.size();
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int start = lower_bound(nums, target);
+        if(start == nums.size() || nums[start] != target) {
+            return {-1, -1};
+        }
+        int end = lower_bound(nums, target + 1) - 1;
+        return {start, end};
+    }
+};
+
+class Solution {
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
         if(!nums.size()) return {-1, -1};
         int l = 0, r = nums.size()-1;
