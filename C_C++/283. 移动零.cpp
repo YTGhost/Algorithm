f56bdb9bdb4eb8eab6603dfc0ee42e6e2c80af0f
Liang Deng <283304489@qq.com>
2024-01-17 22:06:25 +0800

    modify 283 2024/01/17

--- C_C++/283. 移动零.cpp
+++ C_C++/283. 移动零.cpp
@@ -2,6 +2,21 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
+        int l = 0, r = 0;
+        while(r < n) {
+            if(nums[r]) {
+                swap(nums[l], nums[r]);
+                l++;
+            }
+            r++;
+        }
+    }
+};
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
         for(int i = 0, j = 0; j < n; j++) {
             if(nums[j] != 0) {
                 swap(nums[j], nums[i]);
