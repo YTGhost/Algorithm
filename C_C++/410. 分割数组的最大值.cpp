2f59348f5ad601248a823537255eab6b131d8315
Liang Deng <283304489@qq.com>
2024-01-21 01:18:14 +0800

    modify 410 2024/01/21

--- C_C++/410. 分割数组的最大值.cpp
+++ C_C++/410. 分割数组的最大值.cpp
@@ -1,5 +1,45 @@
 class Solution {
 public:
+    vector<int> getPrefixSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefixSum(n + 1);
+        for(int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        return prefixSum;
+    }
+    bool check(vector<int>& prefixSum, int k, int t) {
+        int l = 0, r = 0, n = prefixSum.size() - 1, cnt = 0;
+        while(r <= n) {
+            if(prefixSum[r] - prefixSum[l] > t) {
+                l = r - 1;
+                cnt++;
+                if(cnt >= k) {
+                    return false;
+                }
+            } else {
+                r++;
+            }
+        }
+        return cnt <= k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        vector<int> prefixSum = getPrefixSum(nums);
+        int l = 0, r = accumulate(nums.begin(), nums.end(), 0) + 1;
+        while(l < r) {
+            int mid = l + (r - l) / 2;
+            if(check(prefixSum, k, mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+};
+
+class Solution {
+public:
     int splitArray(vector<int>& nums, int m) {
         int n = nums.size();
         vector<vector<long long>> f(n + 1, vector<long long>(m + 1, LLONG_MAX));
