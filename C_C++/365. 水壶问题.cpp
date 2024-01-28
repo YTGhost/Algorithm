40db97cfa5bcc64f38744c492c607867d438fe73
Liang Deng <283304489@qq.com>
2024-01-28 23:22:47 +0800

    modify 365 2024/01/28

--- C_C++/365. 水壶问题.cpp
+++ C_C++/365. 水壶问题.cpp
@@ -1,5 +1,21 @@
 class Solution {
 public:
+    int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    bool canMeasureWater(int jug1Capacity, int jug2Capacity, int targetCapacity) {
+        if(jug1Capacity + jug2Capacity < targetCapacity) {
+            return false;
+        }
+        if(jug1Capacity == 0 || jug2Capacity == 0) {
+            return targetCapacity == 0 || jug1Capacity + jug2Capacity == 0;
+        }
+        return targetCapacity % gcd(jug1Capacity, jug2Capacity) == 0;
+    }
+};
+
+class Solution {
+public:
     bool canMeasureWater(int x, int y, int z) {
         if(x + y < z)   return false;
         if(x == 0 || y == 0)    return z == 0 || x + y == z;
