6d329e726e3bccfefdb6824d4bf53cdbfa1a260a
Liang Deng <283304489@qq.com>
2024-01-29 23:58:37 +0800

    modify 994 2024/01/29

--- C_C++/994. 腐烂的橘子.cpp
+++ C_C++/994. 腐烂的橘子.cpp
@@ -1,6 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int res = 0, n = grid.size(), m = grid[0].size(), cnt = 0;
+        queue<pair<int, int>> q;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 2) {
+                    q.push({i, j});
+                } else {
+                    cnt++;
+                }
+            }
+        }
+        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        while(!q.empty()) {
+            int cnt = q.size();
+            for(int i = 0; i < cnt; i++) {
+                auto node = q.front();
+                q.pop();
+                for(int j = 0; j < 4; j++) {
+                    int nx = node.first + dx[j], ny = node.second + dy[j];
+                    if(nx >= 0 && nx < n && ny >= 0 && ny < m && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                    }
+                }
+            }
+            if(q.size()) {
+                res++;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j] == 1) {
+                    return -1;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
         vector<int> p1 = {1, -1, 0, 0};
         vector<int> p2 = {0, 0, 1, -1};
         queue<pair<int, int>> q;
