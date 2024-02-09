262bc87dfaeb44defee48d958b93383c2af70945
Liang Deng <283304489@qq.com>
2024-02-10 00:22:05 +0800

    add 94 2024/02/10

--- C_C++/94. 二叉树的中序遍历.cpp
+++ C_C++/94. 二叉树的中序遍历.cpp
@@ -20,6 +20,21 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
+        function<void(TreeNode*)> dfs = [&](TreeNode* root) {
+            if(!root) return;
+            dfs(root->left);
+            res.push_back(root->val);
+            dfs(root->right);
+        };
+        dfs(root);
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
         function<void(TreeNode* root)> dfs = [&](TreeNode* root) {
             if(!root) return;
             dfs(root->left);
