532fe515419c9c740b0fa24fb6fa7cff193b8318
Liang Deng <283304489@qq.com>
2024-02-16 00:13:16 +0800

    modify 103 2024/02/16

--- C_C++/103. 二叉树的锯齿形层序遍历.cpp
+++ C_C++/103. 二叉树的锯齿形层序遍历.cpp
@@ -13,6 +13,35 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> res;
+        if(!root) return res;
+        queue<TreeNode*> q;
+        int level = 0;
+        q.push(root);
+        while(!q.empty()) {
+            int cnt = q.size();
+            deque<int> list;
+            for(int i = 0; i < cnt; i++) {
+                auto node = q.front();
+                q.pop();
+                if(level % 2 == 0) {
+                    list.push_back(node->val);
+                } else {
+                    list.push_front(node->val);
+                }
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            res.push_back(vector<int>{list.begin(), list.end()});
+            level++;
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        vector<vector<int>> res;
         bool flag = true;
         if(!root) return res;
         queue<TreeNode*> q;
