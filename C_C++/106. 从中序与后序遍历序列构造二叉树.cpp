ad055f25801aaaab818197e2165bbbd78aa21eb2
Liang Deng <283304489@qq.com>
2024-02-21 00:43:18 +0800

    modify 106 2024/02/21

--- C_C++/106. 从中序与后序遍历序列构造二叉树.cpp
+++ C_C++/106. 从中序与后序遍历序列构造二叉树.cpp
@@ -10,6 +10,22 @@
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        if(postorder.empty()) return NULL;
+        int n = postorder.size();
+        int leftSize = ranges::find(inorder, postorder[n - 1]) - inorder.begin();
+        vector<int> ino1(inorder.begin(), inorder.begin() + leftSize);
+        vector<int> ino2(inorder.begin() + 1 + leftSize, inorder.end());
+        vector<int> pos1(postorder.begin(), postorder.begin() + leftSize);
+        vector<int> pos2(postorder.begin() + leftSize, postorder.end() - 1);
+        TreeNode* left = buildTree(ino1, pos1);
+        TreeNode* right = buildTree(ino2, pos2);
+        return new TreeNode(postorder[n - 1], left, right);
+    }
+};
+
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
         return build(0, inorder.size()-1, 0, postorder.size()-1, inorder, postorder);
     }
     TreeNode* build(int inleft, int inright, int posleft, int posright, vector<int>& inorder, vector<int>& postorder){
