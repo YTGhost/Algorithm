5cfceec77d2587c40e39e160a6c057c86222f485
Liang Deng <283304489@qq.com>
2024-02-25 01:06:45 +0800

    modify 235 2024/02/25

--- C_C++/235. 二叉搜索树的最近公共祖先.cpp
+++ C_C++/235. 二叉搜索树的最近公共祖先.cpp
@@ -20,6 +20,20 @@ public:
         return root;
     }
 };
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int x = root->val;
+        if(p->val < x && q->val < x) {
+            return lowestCommonAncestor(root->left, p, q);
+        }
+        if(p->val > x && q->val > x) {
+            return lowestCommonAncestor(root->right, p, q);
+        }
+        return root;
+    }
+};
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
