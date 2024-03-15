70a47df4e96390dbb5f017c33aa39f406112fb38
Liang Deng <283304489@qq.com>
2024-03-15 22:57:48 +0800

    modify lc 73 2024/03/15

--- LeetCode/C_C++/73. 矩阵置零.cpp
+++ LeetCode/C_C++/73. 矩阵置零.cpp
@@ -1,6 +1,48 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        bool flag_row0 = false, flag_col0 = false;
+        int m = matrix.size(), n = matrix[0].size();
+        for(int i = 0; i < n; i++) {
+            if(matrix[0][i] == 0) {
+                flag_row0 = true;
+            }
+        }
+        for(int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                flag_col0 = true;
+            }
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(!matrix[i][j]) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(!matrix[i][0] || !matrix[0][j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(flag_row0) {
+            for(int i = 0; i < n; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(flag_col0) {
+            for(int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+};
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
         int flag_col0 = false, flag_row0 = false;
