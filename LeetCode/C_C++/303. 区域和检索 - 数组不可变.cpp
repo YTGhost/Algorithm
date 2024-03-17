3074326a8618c60e2f1f8986c343f1a7fef4fc95
Liang Deng <283304489@qq.com>
2024-03-18 00:10:17 +0800

    modify lc 303 2024/03/18

--- LeetCode/C_C++/303. 区域和检索 - 数组不可变.cpp
+++ LeetCode/C_C++/303. 区域和检索 - 数组不可变.cpp
@@ -1,5 +1,25 @@
 class NumArray {
 public:
+    vector<int> prefixSum;
+    vector<int> getPrefixSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> prefixSum(n + 1);
+        for(int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        return prefixSum;
+    }
+    NumArray(vector<int>& nums) {
+        prefixSum = getPrefixSum(nums);
+    }
+    
+    int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+};
+
+class NumArray {
+public:
     vector<int> pre;
     NumArray(vector<int>& nums) {
         int n = nums.size();
