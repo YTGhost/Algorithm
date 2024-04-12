6478b749bf0bf06bb902e65da525167443322ce2
Liang Deng <283304489@qq.com>
2024-04-12 16:40:49 +0800

    modify lc 2923 2024/04/12

--- LeetCode/C_C++/2923. 找到冠军 I.cpp
+++ LeetCode/C_C++/2923. 找到冠军 I.cpp
@@ -2,6 +2,20 @@ class Solution {
 public:
     int findChampion(vector<vector<int>>& grid) {
         int n = grid.size();
+        int res = 0;
+        for(int i = 1; i < n; i++) {
+            if(grid[i][res] == 1) {
+                res = i;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int n = grid.size();
         unordered_set<int> s;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
