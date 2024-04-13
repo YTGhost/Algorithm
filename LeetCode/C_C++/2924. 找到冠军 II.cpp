1fd9ebef8a2d069c72d72e559722f47f1d8b27de
Liang Deng <283304489@qq.com>
2024-04-13 23:00:00 +0800

    modify lc 2924 2024/04/13

--- LeetCode/C_C++/2924. 找到冠军 II.cpp
+++ LeetCode/C_C++/2924. 找到冠军 II.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int>hash(n);
+        for(auto edge : edges) {
+            hash[edge[1]]++;
+        }
+        int cnt = 0, res = -1;
+        for(int i = 0; i < n; i++) {
+            if(!hash[i]) {
+                cnt++;
+                if(cnt > 1) {
+                    return -1;
+                }
+                res = i;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
         vector<int> res;
         unordered_map<int, int> m;
         for(int i = 0; i < edges.size(); i++) {
