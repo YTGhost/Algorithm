6c844c5c39c499d567f7a267b03c2c1a76879c81
Liang Deng <283304489@qq.com>
2024-05-29 00:35:36 +0800

    add lc 2981 2024/05/29

--- LeetCode/C_C++/2981. 找出出现至少三次的最长特殊子字符串 I.cpp
+++ LeetCode/C_C++/2981. 找出出现至少三次的最长特殊子字符串 I.cpp
@@ -1,5 +1,30 @@
 class Solution {
 public:
+    int maximumLength(string s) {
+        int n = s.length();
+        vector<vector<int>> hash(26);
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            cnt++;
+            if(i + 1 == n || s[i] != s[i + 1]) {
+                hash[s[i] - 'a'].push_back(cnt);
+                cnt = 0;
+            }
+        }
+        int res = 0;
+        for(auto &arr : hash) {
+            if(arr.empty()) continue;
+            sort(arr.rbegin(), arr.rend());
+            arr.push_back(0);
+            arr.push_back(0);
+            res = max({res, arr[0] - 2, min(arr[0] - 1, arr[1]), arr[2]});
+        }
+        return res == 0 ? -1 : res;
+    }
+};
+
+class Solution {
+public:
     bool check(string &s, int l, int r) {
         unordered_map<char, int> hash;
         for(int i = l; i <= r; i++) {
