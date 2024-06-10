70f84a8989a6700cb1755f84f26524f5808ab114
Liang Deng <283304489@qq.com>
2024-06-11 00:09:03 +0800

    modify lc 419 2024/06/11

--- LeetCode/C_C++/419. 甲板上的战舰.cpp
+++ LeetCode/C_C++/419. 甲板上的战舰.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
     int countBattleships(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n));
+        int res = 0;
+        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        function<void(int, int)> dfs = [&](int x, int y) {
+            visited[x][y] = true;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i], ny = y + dy[i];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && board[nx][ny] == 'X' && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        };
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X' && !visited[i][j]) {
+                    res++;
+                    dfs(i, j);
+                }
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
         int n = board.size(), m =  board[0].size(), res = 0;
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
