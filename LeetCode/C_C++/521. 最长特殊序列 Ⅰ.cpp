d5dc6c8de365ca5d2daef04672754247e164dcf4
Liang Deng <283304489@qq.com>
2024-06-16 20:54:04 +0800

    modify lc 521 2024/06/16

--- LeetCode/C_C++/521. 最长特殊序列 Ⅰ.cpp
+++ LeetCode/C_C++/521. 最长特殊序列 Ⅰ.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     int findLUSlength(string a, string b) {
+        return a != b ? max(a.length(), b.length()) : -1;
+    }
+};
+
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
         return a == b ? -1 : max(a.length(), b.length());
     }
 };
