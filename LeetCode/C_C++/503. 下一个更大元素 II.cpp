786b010a87b6d1013a55a1e109d4a4b97d7eca0d
Liang Deng <283304489@qq.com>
2024-06-24 01:07:58 +0800

    modify 503 2024/06/24

--- LeetCode/C_C++/503. 下一个更大元素 II.cpp
+++ LeetCode/C_C++/503. 下一个更大元素 II.cpp
@@ -4,6 +4,23 @@ public:
         int n = nums.size();
         vector<int> res(n, -1);
         stack<int> stk;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            while (!stk.empty() && nums[stk.top()] < nums[i % n]) {
+                res[stk.top()] = nums[i % n];
+                stk.pop();
+            }
+            stk.push(i % n);
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n, -1);
+        stack<int> stk;
         for(int i = 0; i < 2 * n - 1; i++) {
             while(!stk.empty() && nums[stk.top()] < nums[i % n]) {
                 res[stk.top()] = nums[i % n];
