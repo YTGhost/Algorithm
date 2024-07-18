947763e1cbf539cb2d6f7cfadbe4dcbc1ad30fd5
Liang Deng <283304489@qq.com>
2024-07-19 00:12:29 +0800

    modify lc 3096 2024/07/19

--- LeetCode/C_C++/3096. 得到更多分数的最少关卡数目.cpp
+++ LeetCode/C_C++/3096. 得到更多分数的最少关卡数目.cpp
@@ -2,6 +2,25 @@ class Solution {
 public:
     int minimumLevels(vector<int>& possible) {
         int n = possible.size();
+        vector<int> prefixSum(n + 1);
+        for (int i = 0; i < n; i++) {
+            prefixSum[i + 1] = prefixSum[i] + (possible[i] == 0 ? -1 : 1);
+        }
+        int res = -1;
+        for (int i = 1; i < n; i++) {
+            if (prefixSum[i] > prefixSum[n] - prefixSum[i]) {
+                res = i;
+                break;
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int minimumLevels(vector<int>& possible) {
+        int n = possible.size();
         for(int i = 0; i < n; i++) {
             if (possible[i] == 0) {
                 possible[i] = -1;
