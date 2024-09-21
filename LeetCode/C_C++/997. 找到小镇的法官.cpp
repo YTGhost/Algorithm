93dd28ec5b2d78d1fe7492c794c12182fd31d630
Liang Deng <283304489@qq.com>
2024-09-22 00:37:54 +0800

    modify lc 997 2024/09/22

--- LeetCode/C_C++/997. 找到小镇的法官.cpp
+++ LeetCode/C_C++/997. 找到小镇的法官.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> trusted(n + 1);
+        vector<bool> toTrust(n + 1);
+        for (auto item : trust) {
+            toTrust[item[0]] = true;
+            trusted[item[1]]++;
+        }
+        for (int i = 1; i <= n; i++) {
+            if (!toTrust[i] && trusted[i] == n - 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
         unordered_map<int, int> m1, m2;
         for(auto item : trust) {
             m1[item[0]]++;
