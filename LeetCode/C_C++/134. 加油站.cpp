96593ffdf7af77c031e00bf2c1b7863cfe6f96ea
Liang Deng <283304489@qq.com>
2024-10-06 16:06:05 +0800

    modify lc 134 2024/10/06

--- LeetCode/C_C++/134. 加油站.cpp
+++ LeetCode/C_C++/134. 加油站.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int res = 0, s = 0, minS = 0;
+        int n = gas.size();
+        for (int i = 0; i < n; i++) {
+            s += gas[i] - cost[i];
+            if (s < minS) {
+                minS = s;
+                res = i + 1;
+            }
+        }
+        return s < 0 ? -1 : res;
+    }
+};
+
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int run = 0, rest = 0, start = 0;
         for(int i = 0; i < gas.size(); i++) {
             run += gas[i] - cost[i];
