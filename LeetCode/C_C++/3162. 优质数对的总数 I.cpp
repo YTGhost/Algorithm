dc084ece0a8e68281d989d0575844d656ae8072b
Liang Deng <283304489@qq.com>
2024-10-10 12:14:55 +0800

    add lc 3162 2024/10/10

---
+++ LeetCode/C_C++/3162. 优质数对的总数 I.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size(), m = nums2.size(), res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (nums1[i] % (nums2[j] * k) == 0) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
