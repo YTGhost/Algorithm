8fca2cc0c5dc703e9fa78d1c443d29bf42097191
Liang Deng <ytdengliang@gmail.com>
2024-10-27 15:38:37 +0800

    modify lc 684 2024/10/27

--- LeetCode/C_C++/684. 冗余连接.cpp
+++ LeetCode/C_C++/684. 冗余连接.cpp
@@ -1,5 +1,31 @@
 class Solution {
 public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        vector<int> p(n + 1);
+        for (int i = 1; i <= n; i++) {
+            p[i] = i;
+        }
+        function<int(int)> find = [&](int x) {
+            if (p[x] != x) {
+                p[x] = find(p[x]);
+            }
+            return p[x];
+        };
+        vector<int> res;
+        for (auto &edge : edges) {
+            if (find(edge[0]) != find(edge[1])) {
+                p[find(edge[0])] = find(edge[1]);
+            } else {
+                res = edge;
+                break;
+            }
+        }
+        return res;
+    }
+};
+class Solution {
+public:
     vector<int> p;
     int find(vector<vector<int>>& edges, int x) {
         if(p[x] != x) p[x] = find(edges, p[x]);
