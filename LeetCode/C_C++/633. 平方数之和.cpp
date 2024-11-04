64c660549f1d1597a6c101b90286bfe6ba98768b
Liang Deng <ytdengliang@gmail.com>
2024-11-04 14:48:54 +0800

    modify lc 633 2024/11/04

--- LeetCode/C_C++/633. 平方数之和.cpp
+++ LeetCode/C_C++/633. 平方数之和.cpp
@@ -1,6 +1,19 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
+        for (int a = 0; a * a <= c / 2; a++) {
+            int b = sqrt(c - a * a);
+            if (a * a + b * b == c) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
         long long l = 0, r = sqrt(c);
         while(l <= r)
         {
