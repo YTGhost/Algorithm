68df6012dc9a4324121285001475b33d5344b2ac
Liang Deng <ytdengliang@gmail.com>
2024-11-05 14:28:27 +0800

    modify lc 3222 2024/11/05

--- LeetCode/C_C++/3222. 求出硬币游戏的赢家.cpp
+++ LeetCode/C_C++/3222. 求出硬币游戏的赢家.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     string losingPlayer(int x, int y) {
+        int t = min(x, y / 4);
+        return t % 2 == 0 ? "Bob" : "Alice";
+    }
+};
+
+class Solution {
+public:
+    string losingPlayer(int x, int y) {
         bool flag = true;
         while (x >= 1 && y >= 4) {
             flag = !flag;
