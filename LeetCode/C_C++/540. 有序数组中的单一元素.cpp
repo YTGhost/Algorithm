5bf460d0fb2b70351bf2c36289d4885effb6b29d
Liang Deng <ytdengliang@gmail.com>
2024-11-10 13:59:20 +0800

    modify lc 540 2024/11/10

--- LeetCode/C_C++/540. 有序数组中的单一元素.cpp
+++ LeetCode/C_C++/540. 有序数组中的单一元素.cpp
@@ -1,6 +1,65 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0, r = n / 2;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid * 2] != nums[mid * 2 + 1]) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return nums[2 * l];
+    }
+};
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) {
+            return nums[0];
+        }
+        int l = 0, r = n;
+        int res;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (mid == 0 && nums[mid] != nums[mid + 1]) {
+                res = nums[mid];
+                break;
+            }
+            if (mid == n - 1 && nums[mid] != nums[mid - 1]) {
+                res = nums[mid];
+                break;
+            }
+            if (mid != 0 && mid != n - 1 && nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                res = nums[mid];
+                break;
+            }
+            if (mid && nums[mid] == nums[mid - 1]) {
+                if (mid % 2 == 0) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+            if (mid != n - 1 && nums[mid] == nums[mid + 1]) {
+                if (mid % 2 == 0) {
+                    l = mid + 2;
+                } else {
+                    r = mid;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
         int l = 0, r = nums.size() - 1, n = nums.size();
         while(l < r) {
             int mid = l + r >> 1;
