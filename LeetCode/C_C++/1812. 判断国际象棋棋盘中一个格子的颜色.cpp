184f556f58c8cdb20503db063f5c7e4613f704c4
Liang Deng <ytdengliang@gmail.com>
2024-12-09 11:05:44 +0800

    modify lc 1812 2024/12/09

--- LeetCode/C_C++/1812. 判断国际象棋棋盘中一个格子的颜色.cpp
+++ LeetCode/C_C++/1812. 判断国际象棋棋盘中一个格子的颜色.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool squareIsWhite(string coordinates) {
+        int a = coordinates[0] - 'a', b = coordinates[1] - '1';
+        return !(a % 2 == b % 2);
+    }
+};
+
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
         int a = coordinates[0] - 'a';
         int b = coordinates[1] - '1';
         if(a % 2 == 0) {
